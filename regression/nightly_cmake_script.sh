880cf35790aab3cbe5822ec9294dbe463f2f1b79
kellyt <kellyt@2e4270bf-7bdd-41b3-a9ef-8cbd4d332402>
2010-12-14 17:51:49 +0000

    + Patch up local environment for regress account.

--- regression/nightly_cmake_script.sh
+++ regression/nightly_cmake_script.sh
@@ -5,7 +5,21 @@
 #      source/  <-- Source files checked out from CVS go here.
 #      build/   <-- Make is run in this location.
 
+# Environment setup
+
 unset http_proxy
+export VENDOR_DIR=/ccs/codes/radtran/vendors/Linux64
+
+if test -z "$MODULESHOME"; then
+  # This is a new login
+  if test -f /home/regress/environment/Modules/init/bash; then
+    source /home/regress/environment/Modules/init/bash
+    module load grace BLACS SCALAPACK SuperLU_DIST/2.4 gandolf gcc/4.3.4 gsl hypre
+    module load lapack ndi openmpi ParMetis/3.1.1 trilinos/10.4.0
+    module list
+  fi
+fi
+
 
 # Run the ctest (regression) script.  This script will take the following build steps: 
 # 1. cvs update
@@ -21,14 +35,14 @@ unset http_proxy
 dashboard_type=Nightly
 
 export work_dir=/home/regress/cmake_draco/Nightly/Release
-ctest -VV -S ~/draco/regression/Draco_gcc.cmake,${dashboard_type},Release
+ctest -VV -S /home/regress/cmake_draco/regression/Draco_gcc.cmake,${dashboard_type},Release
 
-# # Debug build
+# Debug build
 export work_dir=/home/regress/cmake_draco/Nightly/Debug
-ctest -VV -S ~/draco/regression/Draco_gcc.cmake,${dashboard_type},Debug
+ctest -VV -S /home/regress/cmake_draco/regression/Draco_gcc.cmake,${dashboard_type},Debug
 
 # Coverage build
 module load bullseyecoverage
 export work_dir=/home/regress/cmake_draco/Nightly/Coverage
-ctest -VV -S ~/draco/regression/Draco_gcc.cmake,${dashboard_type},Debug,Coverage
+ctest -VV -S /home/regress/cmake_draco/regression/Draco_gcc.cmake,${dashboard_type},Debug,Coverage
 module unload bullseyecoverage
