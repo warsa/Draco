1e1711ec0954607ef2a8463df5180f75faf42d64
kellyt <kellyt@2e4270bf-7bdd-41b3-a9ef-8cbd4d332402>
2011-01-02 19:30:26 +0000

    + Provide a file with the full LANS Copyright statement and ensure   that it is installed. + Provide a ToDo file that provides a placeholder for bugs, plans,   etc. + Move some cmake code from the main CMakeLists.txt to   config/dracoTesting.cmake. + Minor tweak to the dracoVersion file to fix some code that broke   when we transitioned to draco-6_0_0.

---
+++ config/dracoTesting.cmake
@@ -0,0 +1,34 @@
+# File: dracoTesting.cmake
+
+option( DRACO_BUILD_TESTS "Should we compile the tests?" ON )
+
+# how many cores on the local system?
+if( UNIX )
+  if( EXISTS "/proc/cpuinfo" )
+    file( READ "/proc/cpuinfo" cpuinfo )
+    # convert one big string into a set of strings, one per line
+    string( REGEX REPLACE "\n" ";" cpuinfo ${cpuinfo} )
+    set( proc_ids "" )
+    foreach( line ${cpuinfo} )
+       if( ${line} MATCHES "processor" )
+          list( APPEND proc_ids ${line} )
+       endif()
+    endforeach()
+    list( LENGTH proc_ids DRACO_NUM_CORES )
+    set( DRACO_NUM_CORES ${DRACO_NUM_CORES} CACHE STRING 
+       "Number of cores on the local machine." )
+  endif()
+endif()
+
+# enable ctest funcitons and run ctest in parallel if we have multiple cores.
+if( DRACO_BUILD_TESTS )
+  include(CTest)
+  enable_testing()
+  if( "${DRACO_NUM_CORES}none" STREQUAL "none"  )
+     add_custom_target( check
+        COMMAND ${CMAKE_MAKE_COMMAND} test )   
+  else()
+     add_custom_target( check
+        COMMAND ${CMAKE_CTEST_COMMAND} -j ${DRACO_NUM_CORES} )
+  endif()
+endif()  
--- config/dracoVersion.cmake
+++ config/dracoVersion.cmake
@@ -10,31 +10,39 @@
 # ${DRACO_DATE_STAMP_MONTH}
 # ${DRACO_DATE_STAMP_DAY}
 
-set(DRACO_VERSION_MAJOR 6)
-set(DRACO_VERSION_MINOR 0)
-set(DRACO_VERSION_PATCH 0)
+if( "${DRACO_VERSION_MAJOR}notset" STREQUAL "notset" )
+   message( WARNING "DRACO_VERSION_MAJOR should already be set!" )
+   set(DRACO_VERSION_MAJOR 0)
+endif()
+if( "${DRACO_VERSION_MINOR}notset" STREQUAL "notset" )
+   message( WARNING "DRACO_VERSION_MINOR should already be set!" )
+   set(DRACO_VERSION_MINOR 0)
+endif()
 
+# Use the configure date as the patch number for development builds
+# (non release builds)
 if( UNIX )
-    execute_process( COMMAND date +%m/%d/%Y
-       OUTPUT_VARIABLE configureDate )
-endif()
-	
-if( WIN32 )
-  execute_process( COMMAND "cmd" "/c" "date" "/t"
-	OUTPUT_VARIABLE configureDate )
+   execute_process( 
+      COMMAND         date +%m/%d/%Y
+      OUTPUT_VARIABLE configureDate )
+elseif( WIN32 )
+   execute_process( 
+      COMMAND         "cmd" "/c" "date" "/t"
+      OUTPUT_VARIABLE configureDate )
 endif()
 
-  string( REGEX REPLACE 
-     ".*([0-9][0-9])/([0-9][0-9])/(20[0-9][0-9]).*"
-     "\\3" DRACO_DATE_STAMP_YEAR "${configureDate}" )
-  string( REGEX REPLACE 
-     ".*([0-9][0-9])/([0-9][0-9])/(20[0-9][0-9]).*"
-     "\\1" DRACO_DATE_STAMP_MONTH "${configureDate}" )
-  string( REGEX REPLACE 
-     ".*([0-9][0-9])/([0-9][0-9])/(20[0-9][0-9]).*"
-     "\\2" DRACO_DATE_STAMP_DAY "${configureDate}" )
+# Format the configureDate
+string( REGEX REPLACE 
+   ".*([0-9][0-9])/([0-9][0-9])/(20[0-9][0-9]).*"
+   "\\3" DRACO_DATE_STAMP_YEAR "${configureDate}" )
+string( REGEX REPLACE 
+   ".*([0-9][0-9])/([0-9][0-9])/(20[0-9][0-9]).*"
+   "\\1" DRACO_DATE_STAMP_MONTH "${configureDate}" )
+string( REGEX REPLACE 
+   ".*([0-9][0-9])/([0-9][0-9])/(20[0-9][0-9]).*"
+   "\\2" DRACO_DATE_STAMP_DAY "${configureDate}" )
 
-if( "${DRACO_VERSION_MINOR}" MATCHES "[1-9]?[1-9]$")
+if( "${DRACO_VERSION_PATCH}notset" STREQUAL "notset" ) # "[1-9]?[1-9]$")
    set( DRACO_VERSION_PATCH
       "${DRACO_DATE_STAMP_YEAR}${DRACO_DATE_STAMP_MONTH}${DRACO_DATE_STAMP_DAY}"
       )
@@ -47,3 +55,8 @@ set( DRACO_VERSION_FULL  "${DRACO_VERSION}.${DRACO_VERSION_PATCH}"
 
 message( STATUS "This is Draco version ${DRACO_VERSION_FULL}.")
 
+
+# Support for CPack
+set( CPACK_PACKAGE_VERSION_MAJOR ${DRACO_VERSION_MAJOR} )
+set( CPACK_PACKAGE_VERSION_MINOR ${DRACO_VERSION_MINOR} )
+set( CPACK_PACKAGE_VERSION_PATCH ${DRACO_VERSION_PATCH} )
