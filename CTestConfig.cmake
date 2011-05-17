0b8f26c4608a28b8d905aa93889eedd9c9e1f0c9
kellyt <kellyt@2e4270bf-7bdd-41b3-a9ef-8cbd4d332402>
2011-05-17 16:25:47 +0000

    + Fix NIGHTLY_START_TIME so that the cron initiated regression script   starts after this tagged CVS checkout time.

--- CTestConfig.cmake
+++ CTestConfig.cmake
@@ -5,10 +5,10 @@
 ##   ENABLE_TESTING()
 ##   INCLUDE(CTest)
 set( CTEST_PROJECT_NAME "Draco" )
-set( CTEST_NIGHTLY_START_TIME "00:10:00 MST" )
+set( CTEST_NIGHTLY_START_TIME "00:0:01 MST" )
 
 set( CTEST_DROP_METHOD "http")
 set( CTEST_DROP_SITE "coder.lanl.gov" )
-set( CTEST_DROP_LOCATION  "/cdash/submit.php?project=Draco" )
+set( CTEST_DROP_LOCATION  "/cdash/submit.php?project=${CTEST_PROJECT_NAME}" )
 set( CTEST_DROP_SITE_CDASH TRUE )
 
