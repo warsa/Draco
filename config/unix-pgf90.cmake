8d8de1a5a11235bfd9ac59eec05d1958abcb9fc5
kellyt <kellyt@2e4270bf-7bdd-41b3-a9ef-8cbd4d332402>
2011-06-07 18:25:40 +0000

    + Provide default compiler flags for pgf90.

---
+++ config/unix-pgf90.cmake
@@ -0,0 +1,28 @@
+#-----------------------------*-cmake-*----------------------------------------#
+# file   config/unix-pgf90.cmake
+# author Kelly Thompson 
+# date   2011 June 7
+# brief  Establish flags for Unix - PGI Fortran
+# note   Copyright © 2010 Los Alamos National Security, LLC.
+#        All rights reserved.
+#------------------------------------------------------------------------------#
+# $Id$
+#------------------------------------------------------------------------------#
+
+# Let anyone who is interested in which FORTRAN compiler we're using
+# switch on this macro.
+set( CMAKE_Fortran_COMPILER_FLAVOR "PGI" )
+
+set( CMAKE_Fortran_FLAGS 
+  "-Mfreeform"  ) 
+set( CMAKE_Fortran_FLAGS_DEBUG 
+  "-g -Mbounds -Mchkptr")
+set( CMAKE_Fortran_FLAGS_RELEASE 
+  "-O3")
+set( CMAKE_Fortran_FLAGS_MINSIZEREL "${CMAKE_Fortran_FLAGS_RELEASE}" )
+set( CMAKE_Fortran_FLAGS_RELWITHDEBINFO 
+  "${CMAKE_Fortran_FLAGS_DEBUG} -O3")
+
+#------------------------------------------------------------------------------#
+# End config/unix-pgf90.cmake
+#------------------------------------------------------------------------------#
