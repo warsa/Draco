1e05d01a9f7463655c05a2822bbcdac1c917748d
kellyt <kellyt@2e4270bf-7bdd-41b3-a9ef-8cbd4d332402>
2011-07-25 18:51:14 +0000

    + Remove commented, deprecated code (left over from copying   unix-g++.cmake).

--- config/unix-ppu.cmake
+++ config/unix-ppu.cmake
@@ -8,75 +8,6 @@
 #------------------------------------------------------------------------------#
 # $Id$
 #------------------------------------------------------------------------------#
-
-#
-# Sanity Checks
-# 
-
-# Must use static libraries.
-# See DRACO_LIBRARY_TYPE
-# set( DRACO_LIBRARY_TYPE "STATIC" CACHE STRING 
-#    "Keyword for creating new libraries (STATIC or SHARED)."
-#    FORCE )
-
-#
-# C++ libraries required by Fortran linker
-# 
-
-# execute_process( 
-#   COMMAND ${CMAKE_C_COMPILER} -print-libgcc-file-name
-#   TIMEOUT 5
-#   RESULT_VARIABLE tmp
-#   OUTPUT_VARIABLE libgcc_path
-#   ERROR_VARIABLE err
-#   )
-# get_filename_component( libgcc_path ${libgcc_path} PATH )
-# execute_process( 
-#   COMMAND ${CMAKE_CXX_COMPILER} -print-file-name=libstdc++.so
-#   TIMEOUT 5
-#   RESULT_VARIABLE tmp
-#   OUTPUT_VARIABLE libstdcpp_so_loc
-#   ERROR_VARIABLE err
-#   OUTPUT_STRIP_TRAILING_WHITESPACE
-#   )
-# get_filename_component( libstdcpp_so_loc ${libstdcpp_so_loc} ABSOLUTE )
-# execute_process( 
-#   COMMAND ${CMAKE_CXX_COMPILER} -print-file-name=libgcc_s.so
-#   TIMEOUT 5
-#   RESULT_VARIABLE tmp
-#   OUTPUT_VARIABLE libgcc_s_so_loc
-#   ERROR_VARIABLE err
-#   OUTPUT_STRIP_TRAILING_WHITESPACE
-#   )
-# get_filename_component( libgcc_s_so_loc ${libgcc_s_so_loc} ABSOLUTE )
-# set( GCC_LIBRARIES 
-#   ${libstdcpp_so_loc}
-#   ${libgcc_s_so_loc}
-#   )
-#message(   "   - GNU C++  : ${libstdcpp_so_loc}" )
-#message(   "   -          : ${libgcc_s_so_loc}" )
-
-#
-# config.h settings
-#
-
-# execute_process(
-#   COMMAND ${CMAKE_C_COMPILER} --version
-#   OUTPUT_VARIABLE ABS_C_COMPILER_VER
-#   )
-# string( REGEX REPLACE "Copyright.*" " " 
-#   ABS_C_COMPILER_VER ${ABS_C_COMPILER_VER} )
-# string( STRIP ${ABS_C_COMPILER_VER} ABS_C_COMPILER_VER )
-
-# execute_process(
-#   COMMAND ${CMAKE_CXX_COMPILER} --version
-#   OUTPUT_VARIABLE ABS_CXX_COMPILER_VER
-#   )
-# string( REGEX REPLACE "Copyright.*" " " 
-#   ABS_CXX_COMPILER_VER ${ABS_CXX_COMPILER_VER} )
-# string( STRIP ${ABS_CXX_COMPILER_VER} ABS_CXX_COMPILER_VER )
-
-
 #
 # Compiler Flags
 # 
@@ -89,21 +20,6 @@
 # -D_GLIBCXX_DEBUG -D_GLIBCXX_DEBUG_PEDANTIC
 # -Wextra
 # -Weffc++
-
-# /opt/cell/toolchain/bin/ppu-g++ -o Release.o -c -W -Wall
-# -Wno-sign-compare -m64 -mabi=altivec -maltivec -O0 -D__PPU__
-# -DADDRESSING_64 -DDBC=7 -gdwarf-2
-
-# -DMESH_EVERY_CYCLE 
-# -DHOST_ACCEL_DACS 
-# -DPPE_WRITE_BUFFER_DIRECT 
-# -DPPE_READ_BUFFER_DIRECT 
-# -DACCEL_RECV_NONBLOCKING 
-# -DACCEL_SEND_NONBLOCKING 
-# -DSHORT_SPE_TALLIES 
-# -DCACHE_LINE_SIZE=128 
-# -Ids++ Release.cc
-
 
 if( CMAKE_GENERATOR STREQUAL "Unix Makefiles" )
 
