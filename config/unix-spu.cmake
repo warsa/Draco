e287654735f13fe1743ecb303ae7e4fee21cf9cb
kellyt <kellyt@2e4270bf-7bdd-41b3-a9ef-8cbd4d332402>
2011-07-25 18:53:17 +0000

    + Provide some comments and reference for specialized g++ inline   parameters. + Tweak the value for the parameter 'large-function-growth' (change   from 1700 to 2850).  This should eliminate many compile warnings and   improve code performance.  The downside is that compile time will be   a bit longer.

--- config/unix-spu.cmake
+++ config/unix-spu.cmake
@@ -10,77 +10,29 @@
 #------------------------------------------------------------------------------#
 
 #
-# Sanity Checks
-# 
-
-#
-# C++ libraries required by Fortran linker
-# 
-
-# execute_process( 
-#   COMMAND ${CMAKE_C_COMPILER} -print-libgcc-file-name
-#   TIMEOUT 5
-#   RESULT_VARIABLE tmp
-#   OUTPUT_VARIABLE libgcc_path
-#   ERROR_VARIABLE err
-#   )
-# get_filename_component( libgcc_path ${libgcc_path} PATH )
-# execute_process( 
-#   COMMAND ${CMAKE_CXX_COMPILER} -print-file-name=libstdc++.so
-#   TIMEOUT 5
-#   RESULT_VARIABLE tmp
-#   OUTPUT_VARIABLE libstdcpp_so_loc
-#   ERROR_VARIABLE err
-#   OUTPUT_STRIP_TRAILING_WHITESPACE
-#   )
-# get_filename_component( libstdcpp_so_loc ${libstdcpp_so_loc} ABSOLUTE )
-# execute_process( 
-#   COMMAND ${CMAKE_CXX_COMPILER} -print-file-name=libgcc_s.so
-#   TIMEOUT 5
-#   RESULT_VARIABLE tmp
-#   OUTPUT_VARIABLE libgcc_s_so_loc
-#   ERROR_VARIABLE err
-#   OUTPUT_STRIP_TRAILING_WHITESPACE
-#   )
-# get_filename_component( libgcc_s_so_loc ${libgcc_s_so_loc} ABSOLUTE )
-# set( GCC_LIBRARIES 
-#   ${libstdcpp_so_loc}
-#   ${libgcc_s_so_loc}
-#   )
-#message(   "   - GNU C++  : ${libstdcpp_so_loc}" )
-#message(   "   -          : ${libgcc_s_so_loc}" )
-
-#
-# config.h settings
-#
-
-# execute_process(
-#   COMMAND ${CMAKE_C_COMPILER} --version
-#   OUTPUT_VARIABLE ABS_C_COMPILER_VER
-#   )
-# string( REGEX REPLACE "Copyright.*" " " 
-#   ABS_C_COMPILER_VER ${ABS_C_COMPILER_VER} )
-# string( STRIP ${ABS_C_COMPILER_VER} ABS_C_COMPILER_VER )
-
-# execute_process(
-#   COMMAND ${CMAKE_CXX_COMPILER} --version
-#   OUTPUT_VARIABLE ABS_CXX_COMPILER_VER
-#   )
-# string( REGEX REPLACE "Copyright.*" " " 
-#   ABS_CXX_COMPILER_VER ${ABS_CXX_COMPILER_VER} )
-# string( STRIP ${ABS_CXX_COMPILER_VER} ABS_CXX_COMPILER_VER )
-
-
-#
 # Compiler Flags
 # 
+
+# Special inlining is required on the SPU.  See
+# http://gcc.gnu.org/onlinedocs/gcc-4.1.1/gcc/Optimize-Options.html
+# for discussion
+#
+# -finline-limit=n will set:
+#    max-inline-insns-single -> n/2
+#    max-inline-insns-auto   -> n/2
+#    min-inline-insns        -> min(130,n/4)
+#    max-inline-insns-rtl    -> n
 
 if( CMAKE_GENERATOR STREQUAL "Unix Makefiles" )
 
    set( CMAKE_C_FLAGS                "-fstack-check -W -Wall -Winline -fno-exceptions -fno-rtti -march=celledp -DADDRESSING_64 -D__USING_GCC -DEDP -DCACHE_LINE_SIZE=128 -include spu_intrinsics.h" )
 
    set( CMAKE_C_FLAGS_DEBUG          "-O0 -gdwarf-2 -finline-limit=100 --param large-function-growth=100" )
-   set( CMAKE_C_FLAGS_RELEASE        "-O3 -finline-limit=1500 --param large-function-growth=1700" )
+   set( CMAKE_C_FLAGS_RELEASE        "-O3 -finline-limit=1500 --param large-function-growth=2850" )
+   # 2011/07/25 (kt): Looks like we need max-inline-insns-single>800, large-function-growth>2750
+   # Setting max-inline-insns-single to a larger might eliminate
+   # additional warnings about failed inlining, however doing so causes
+   # gcc-4.1.1 to crash
    set( CMAKE_C_FLAGS_MINSIZEREL     "-O3" )
    set( CMAKE_C_FLAGS_RELWITHDEBINFO "-O3 -gdwarf-2 -finline-limit=100 --param large-function-growth=100" )
    
