8978dc903f061f10989e944f4a6e08e7c280991a
kellyt <kellyt@2e4270bf-7bdd-41b3-a9ef-8cbd4d332402>
2011-08-01 19:23:21 +0000

    + Create a helper macro for using pdflatex.

--- config/doc_macros.cmake
+++ config/doc_macros.cmake
@@ -114,11 +114,105 @@ macro( add_latex_doc )
       add_custom_target(${PROJECT_NAME}_pdf # ALL 
          DEPENDS ${dirpdfs} )
       
-      set( allpdfs ${allpdfs};${dirpdfs_tgt} PARENT_SCOPE )
-
+      get_directory_property( pdir PARENT_DIRECTORY )
+      if( EXISTS "${pdir}" )
+         set( allpdfs ${allpdfs};${dirpdfs_tgt} PARENT_SCOPE )
+      endif()
+      
       # Install instructions
       install( FILES ${dirpdfs} DESTINATION doc )
 
    endif(LATEX_COMPILER)
+
+endmacro()
+
+
+##---------------------------------------------------------------------------##
+## Build pdf from latex
+#
+# Given a list of latex source master files create pdf documents, one
+# for each listed latex source file.  Create a makefile target that
+# will be ${docname}.pdf
+#
+# Usage:
+#
+# add_latex_doc( 
+#    SOURCES   "${list_of_latex_files}" 
+#    TEXINPUTS "${PROJECT_SOURCE_DIR}:${PROJECT_SOURCE_DIR}/../../environment/latex"
+#    BIBINPUTS "${PROJECT_SOURCE_DIR}/../../environment/bibfiles"
+# )
+#
+##---------------------------------------------------------------------------##
+macro( add_pdflatex_doc )
+
+   # These become variables of the form ${addscalartests_SOURCES}, etc.
+   parse_arguments( 
+      # prefix
+      ald
+      # list names
+      "SOURCES;BIBINPUTS;TEXINPUTS"
+      # option names
+      "NONE"
+      ${ARGV}
+      )
+
+   set( ald_BIBINPUTS "${ald_BIBINPUTS}:$ENV{BSTINPUTS}" )
+   set( ald_TEXINPUTS "${ald_TEXINPUTS}:$ENV{TEXINPUTS}" )
+
+   # compile commands
+   if(PDFLATEX_COMPILER)
+
+      foreach( fpdoc ${ald_SOURCES} )
+         get_filename_component( doc ${fpdoc} NAME_WE )
+         add_custom_command( 
+            OUTPUT    ${PROJECT_BINARY_DIR}/${doc}.pdf
+            COMMAND   TEXINPUTS=${ald_TEXINPUTS}
+                      ${PDFLATEX_COMPILER} ${PROJECT_SOURCE_DIR}/${doc}.tex
+            COMMAND   BIBINPUTS=${ald_BIBINPUTS}
+                      ${BIBTEX_COMPILER} ${PROJECT_BINARY_DIR}/${doc}.aux
+            COMMAND   TEXINPUTS=${ald_TEXINPUTS}
+                      ${PDFLATEX_COMPILER} ${PROJECT_SOURCE_DIR}/${doc}.tex
+            COMMAND   TEXINPUTS=${ald_TEXINPUTS}
+                      ${PDFLATEX_COMPILER} ${PROJECT_SOURCE_DIR}/${doc}.tex
+            DEPENDS   ${PROJECT_SOURCE_DIR}/${doc}.tex
+            COMMENT   "pdflatex (${doc}.tex --> ${doc}.pdf)"
+            )
+         
+         # register target for each individual pdf file (eg: make draco-3_0_0.pdf)
+         add_custom_target(${doc}.pdf # ALL 
+            DEPENDS   ${PROJECT_BINARY_DIR}/${doc}.pdf
+            )
+         
+         # target to build all pdf files
+         list( APPEND dirpdfs ${PROJECT_BINARY_DIR}/${doc}.pdf )
+         list( APPEND dirpdfs_tgt ${doc}.pdf )
+         
+         # generate a list of extra files to remove during 'make clean'
+         list( APPEND extra_files 
+            ${PROJECT_BINARY_DIR}/${doc}.log
+            ${PROJECT_BINARY_DIR}/${doc}.bbl
+            ${PROJECT_BINARY_DIR}/${doc}.blg
+            ${PROJECT_BINARY_DIR}/${doc}.aux )
+         
+      endforeach()
+
+      # Provide extra instructions for 'make clean'
+      set_directory_properties(
+         PROPERTIES
+         ADDITIONAL_MAKE_CLEAN_FILES "${extra_files}" )
+      
+      # Create a target to generate all pdfs for this directory.
+      add_custom_target(${PROJECT_NAME}_pdflatex # ALL 
+         DEPENDS ${dirpdfs} )
+      
+      get_directory_property( pdir PARENT_DIRECTORY )
+      if( EXISTS "${pdir}" )
+         set( allpdfs ${allpdfs};${dirpdfs_tgt} PARENT_SCOPE )
+      endif()
+      
+      # Install instructions
+      install( FILES ${dirpdfs} DESTINATION doc )
+
+   endif(PDFLATEX_COMPILER)
 
 endmacro()
