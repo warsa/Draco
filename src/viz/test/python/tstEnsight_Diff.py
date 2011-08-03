f4b9bd5a607b0e2dfb864e8e704b30458220af44
kellyt <kellyt@2e4270bf-7bdd-41b3-a9ef-8cbd4d332402>
2011-08-03 15:58:35 +0000

    + Prefix the version number with the string 'draco' to clarify the   version number when viewing the html overview pages from a combined   doxygen document build (e.g.: when looking at clubimc doxygen html,   specify if the package is from draco or clubimc).

--- src/viz/test/python/tstEnsight_Diff.py
+++ src/viz/test/python/tstEnsight_Diff.py
@@ -5,7 +5,7 @@
 ## $Id$
 ###############################################################################
 
-import sys, os, string
+import sys, os, string, re
 
 ##---------------------------------------------------------------------------##
 ## TtstEnsight_Diff.py
@@ -13,18 +13,31 @@ import sys, os, string
 #  Check standard Ensight output with output produced by tstEnsight_Diff
 ##---------------------------------------------------------------------------##
 
+def file_differs(filename):
+    '''
+    Returns true if the keyword "differs" is found in the output from numdiff.
+    '''
+    file = open(filename, 'r')
+    regex = re.compile(r'\bdiffers\b')
+    found = 0
+    for line in file:
+        match = regex.findall(line)
+        if match:
+            found = 1
+    return found
+        
 def file_not_empty(filename):
     '''
     Returns true if filename is not an empty file.
     '''
     file = open(filename, 'r')
-    line = diff_file.readline()
+    line = file.readline()
     if line:
         return 1
     else:
         return 0
 
-prefixes  = ["./testproblem_ensight", "./part_testproblem_ensight"]
+prefixes  = ["testproblem_ensight", "part_testproblem_ensight"]
 postfix   = ".0001"
 dirs      = ["geo", "Temperatures", "Pressure", "Velocity", "Densities"]
 
@@ -37,24 +50,27 @@ for p in prefixes:
         diff_out = d + '.diff'
 
         # diff the output and reference
-        diff_line = "diff %s %s > %s" % (output, ref_out, diff_out)
+        # diff_line = "fc /a %s %s > %s" % (output, ref_out, diff_out)
+        diff_line = "numdiff %s %s > %s" % (output, ref_out, diff_out)
+        print diff_line
         os.system(diff_line)
 
         # print out an error message if we get lines different
-        if file_not_empty(diff_out):
+        #if file_not_empty(diff_out):
+        if file_differs(diff_out):
             print "tstEnsight_Diff Test: Failed in file %s" % output
 
 # check the case file
-casefile = 'testproblem.case'
-caseout  = prefix + '/' + casefile
-casediff = 'case.diff'
+# casefile = 'testproblem.case'
+# caseout  = prefix + '/' + casefile
+# casediff = 'case.diff'
 
-diff_line = "diff %s %s > %s" % (caseout, casefile, casediff)
-os.system(diff_line)
+# diff_line = "diff %s %s > %s" % (caseout, casefile, casediff)
+# os.system(diff_line)
     
-# print out an error message if we get lines different
-if file_not_empty(casediff):
-    print "tstEnsight_Diff Test: Failed in file %s" % caseout
+# # print out an error message if we get lines different
+# if file_not_empty(casediff):
+    # print "tstEnsight_Diff Test: Failed in file %s" % caseout
 
 
 ###############################################################################
