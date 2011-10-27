b119bcf9322c1a4a512b02306a2c32eafc7b5bf6
kellyt <kellyt@2e4270bf-7bdd-41b3-a9ef-8cbd4d332402>
2011-10-27 15:04:09 +0000

    + Eliminate some directories from the search path as they no longer   exist on ceilito.

--- config/Toolchain-catamount.cmake
+++ config/Toolchain-catamount.cmake
@@ -15,15 +15,9 @@ SET(CMAKE_Fortran_COMPILER ftn) # --target=catamount)
 
 # set the search path for the environment coming with the compiler
 # and a directory where you can install your own compiled software
-set(CMAKE_FIND_ROOT_PATH
-    /opt/xt-pe/default
-    /opt/xt-mpt/default/mpich2-64/GP2
-    $ENV{MPICH_DIR}/lib
-# DRACO_DIR?
-# VENDOR_DIR?    
-  )
+set(CMAKE_FIND_ROOT_PATH $ENV{MPICH_DIR}/lib )
 
-# adjust the default behaviour of the FIND_XXX() commands:
+# adjust the default behavior of the FIND_XXX() commands:
 # search headers and libraries in the target environment, search
 # programs in the host environment.  Can be set to NEVER, ONLY or BOTH (default)
 set(CMAKE_FIND_ROOT_PATH_MODE_PROGRAM NEVER)
@@ -33,14 +27,14 @@ set(CMAKE_FIND_ROOT_PATH_MODE_INCLUDE BOTH)
 # Help find vendor software
 set( MPI_INC_DIR $ENV{MPICH_DIR}/include )
 set( MPI_LIB_DIR $ENV{MPICH_DIR}/lib )
-set( DRACO_LIBRARY_TYPE "STATIC" )
-set( MPIEXEC "/usr/bin/aprun" )
+set( DRACO_LIBRARY_TYPE   "STATIC" )
+set( MPIEXEC              "/usr/bin/aprun" )
 set( MPIEXEC_NUMPROC_FLAG "-n")
-set( MPI_C_LIBRARIES      $ENV{MPICH_DIR}/lib/libmpich.a )
-set( MPI_CXX_LIBRARIES    $ENV{MPICH_DIR}/lib/libmpich.a )
+set( MPI_C_LIBRARIES          $ENV{MPICH_DIR}/lib/libmpich.a )
+set( MPI_CXX_LIBRARIES        $ENV{MPICH_DIR}/lib/libmpich.a )
 set( MPI_Fortran_LIBRARIES    $ENV{MPICH_DIR}/lib/libmpich.a )
-set( MPI_C_INCLUDE_PATH   $ENV{MPICH_DIR}/include )
-set( MPI_CXX_INCLUDE_PATH $ENV{MPICH_DIR}/include )
+set( MPI_C_INCLUDE_PATH       $ENV{MPICH_DIR}/include )
+set( MPI_CXX_INCLUDE_PATH     $ENV{MPICH_DIR}/include )
 set( MPI_Fortran_INCLUDE_PATH $ENV{MPICH_DIR}/include )
 
 
