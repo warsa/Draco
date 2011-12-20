06c7b9b661b227069d6ab0ec78bc3d57484995b2
kellyt <kellyt@2e4270bf-7bdd-41b3-a9ef-8cbd4d332402>
2011-12-20 03:05:42 +0000

    + Update module list for regressions.

--- regression/nightly_cmake_script.sh
+++ regression/nightly_cmake_script.sh
@@ -23,8 +23,8 @@ if test -z "$MODULESHOME"; then
   # This is a new login
   if test -f /home/regress/environment/Modules/init/bash; then
     source /home/regress/environment/Modules/init/bash
-    module load grace BLACS SCALAPACK SuperLU_DIST/2.4 gcc/4.5.2 gsl 
-    module load lapack-gcc ndi openmpi ParMetis/3.1.1 trilinos/10.4.0 cmake
+    module load grace BLACS SCALAPACK SuperLU_DIST/2.4 gcc gsl 
+    module load lapack/atlas-3.8.3 ndi openmpi ParMetis/3.1.1 trilinos/10.4.0 cmake
     module load valgrind hypre numdiff
     module list
   fi
