2a289e73efd7c8f44a08fbfebbe6593df8be3220
kellyt <kellyt@2e4270bf-7bdd-41b3-a9ef-8cbd4d332402>
2012-07-12 17:04:51 +0000

    + Add a section for Milagro.

--- regression/metrics_report.sh
+++ regression/metrics_report.sh
@@ -89,10 +89,43 @@ export COVDIRCFG=`pwd`/covclass_cmake.cfg
 cd ../source/src
 covdir
 
+# Jayenne - Milagro
+
+# echo " "
+# echo " "
+# echo "--------------------"
+# echo "Jayenne - Milagro"
+# echo "--------------------"
+# echo " "
+# echo "Lines of code"
+# echo "-------------"
+# cat /home/regress/cmake_jayenne/milagro/Nightly_gcc/Coverage/build/lines-of-code.log
+# echo " "
+# echo "Code coverage"
+# echo "-------------"
+# cd /home/regress/cmake_jayenne/milagro/Nightly_gcc/Coverage/build
+# export COVFILE=`pwd`/CMake.cov
+# export COVDIRCFG=`pwd`/covclass_cmake.cfg
+# cd ../source/src
+# covdir
+
+ # svn diff -r"{2012-01-09}":"{2012-07-09}" --summarize &> ../m_mod_files.log
+ #  num_files_changed=`cat ../m_mod_files.log | awk '{print $2}' | sort -u | wc -l`
+ #   echo $num_files_changed 
+ #  svn log -r"{2012-01-09}":"{2012-07-09}" --verbose &> ../m_msg.log
+ #  number_of_commits=`cat ../m_msg.log | grep "Changed paths:" | wc -l`
+ #  echo $number_of_commits
+ #  files=`find . -name '*.hh' -o -name '*.cc' -o -name '*.txt' -o -name '*.cmake' -o -name '*.in' -o -name '*.h'`
+ #  svn annotate $files > ../m_file_list
+ #  user_list=`cat ../m_file_list | awk '{print $2}' | sort -u`
+ #   echo $user_list 
+ #  for name in $user_list; do numlines=`grep $name ../m_file_list | wc -l`; echo "$numlines: $name"; done > ../author_loc
+ #   cat ../author_loc | sort -rn
+
 # Send the email
 
-#/bin/mailx -s "${subj}" kellyt@lanl.gov < ${logfile}
-/bin/mailx -s "${subj}" jsbrock@lanl.gov barcher@lanl.gov jayenne@lanl.gov < ${logfile}
+/bin/mailx -s "${subj}" kellyt@lanl.gov < ${logfile}
+#/bin/mailx -s "${subj}" jsbrock@lanl.gov barcher@lanl.gov jayenne@lanl.gov < ${logfile}
 
 # jayenne@lanl.gov
 # jsbrock@lanl.gov
