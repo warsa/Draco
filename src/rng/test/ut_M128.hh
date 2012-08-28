630a563ebe3d96e60798121f3a3d10f3ea5f6524
gaber <gaber@2e4270bf-7bdd-41b3-a9ef-8cbd4d332402>
2012-08-28 19:24:50 +0000

    Add Draco-style file comments.

--- src/rng/test/ut_M128.hh
+++ src/rng/test/ut_M128.hh
@@ -1,3 +1,12 @@
+//----------------------------------*-C++-*----------------------------------//
+/*!
+ * \file   rng/test/ut_M128.hh
+ * \author Gabriel Rockefeller
+ * \date   Mon Aug 27 19:29:53 2012
+ * \brief  Header file for ut_M128 test.
+ * \note   Copyright (C) 2012 Los Alamos National Security, LLC
+ */
+//---------------------------------------------------------------------------//
 #if defined __GNUC__
 #pragma GCC system_header
 #endif
