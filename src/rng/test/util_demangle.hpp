fac0c24703138dd13b6ae08b7d692808bf8a7216
gaber <gaber@2e4270bf-7bdd-41b3-a9ef-8cbd4d332402>
2012-09-07 21:11:11 +0000

    Include <typeinfo> to define typeid, for PGI.

--- src/rng/test/util_demangle.hpp
+++ src/rng/test/util_demangle.hpp
@@ -34,6 +34,9 @@ OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 #ifdef __GNUC__
 #include <cxxabi.h>
 #endif
+#ifdef __PGI
+#include <typeinfo>
+#endif
 
 // Every compiler has a demangling library *somewhere*.  Unfortunately, they're
 // all different...
