a3b42a5c0688ff54211ad037bedd5b07cfad4043
kellyt <kellyt@2e4270bf-7bdd-41b3-a9ef-8cbd4d332402>
2013-01-28 23:18:34 +0000

    + Moving regression scripts from jayenne/jayenne-project/regression/   to draco/regression/. refs #108

---
+++ regression/update_regression_scripts.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+# Ensure that the permissions are correct
+umask 0002
+set MYHOSTNAME = `uname -n`
+
+# Establish modules environment and load proper version of svn.
+if test -z "$MODULESHOME"; then
+   if test -f /ccs/codes/radtran/vendors/modules-3.2.9/init/bash; then
+      source /ccs/codes/radtran/vendors/modules-3.2.9/init/bash
+    fi
+fi
+module load svn
+
+# update the scripts directories in /home/regress
+run () {
+    echo $1
+    if ! [ $dry_run ]; then
+       eval $1
+    fi
+}
+
+run "cd /home/regress/draco/config; svn update"
+run "cd /home/regress/draco/regression; svn update"
+run "cd /home/regress/jayenne/regression; svn update"
+run "cd /home/regress/capsaicin/scripts; svn update"
+
