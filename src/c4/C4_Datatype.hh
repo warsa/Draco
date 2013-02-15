7ac514afcef8be90b8b5357db6b6a2f84033d497
kgbudge <kgbudge@2e4270bf-7bdd-41b3-a9ef-8cbd4d332402>
2013-02-15 14:41:29 +0000

    New files needed for last update.

---
+++ src/c4/C4_Datatype.hh
@@ -0,0 +1,45 @@
+//----------------------------------*-C++-*----------------------------------//
+/*!
+ * \file   c4/C4_Datatype.hh
+ * \author Kent G. Budge
+ * \brief  C4_Datatype class definition.
+ * \note   Copyright 2013 The Regents of the University of California.
+ */
+//---------------------------------------------------------------------------//
+// $Id: C4_Datatype.hh 6334 2011-12-23 19:20:49Z warsa $
+//---------------------------------------------------------------------------//
+
+#ifndef c4_C4_Datatype_hh
+#define c4_C4_Datatype_hh
+
+//#include <ds++/Assert.hh>
+
+// C4 package configure
+#include "c4/config.h"
+
+#ifdef C4_MPI
+#include "c4_mpi.h"
+#endif
+
+namespace rtt_c4
+{
+
+#ifdef C4_MPI
+
+typedef MPI_Datatype C4_Datatype;
+
+#else
+
+// If serial, make this a brain-dead type. It won't actually be used.
+
+typedef void *C4_Datatype;
+
+#endif
+
+} // end namespace rtt_c4
+
+#endif // c4_C4_Datatype_hh
+
+//---------------------------------------------------------------------------//
+//              end of c4/C4_Datatype.hh
+//---------------------------------------------------------------------------//
---
+++ src/c4/test/tstVector_Type.cc
@@ -0,0 +1,124 @@
+//----------------------------------*-C++-*----------------------------------//
+/*!
+ * \file   c4/test/tstVector_Type.cc
+ * \author Thomas M. Evans
+ * \date   Tue Apr  2 15:57:11 2002
+ * \brief  Ping Pong communication test.
+ * \note   Copyright (C) 2002-2013 Los Alamos National Security, LLC.
+ *         All rights reserved.
+ */
+//---------------------------------------------------------------------------//
+// $Id: tstVector_Type.cc 6965 2013-01-04 21:54:13Z kellyt $
+//---------------------------------------------------------------------------//
+
+#include "../global.hh"
+
+#include "ds++/Release.hh"
+#include "../ParallelUnitTest.hh"
+
+using namespace std;
+using namespace rtt_c4;
+
+//---------------------------------------------------------------------------//
+// TESTS
+//---------------------------------------------------------------------------//
+
+void test_simple( rtt_dsxx::UnitTest &ut )
+{
+    // Create vector type for (1:2, 2:4) subset of a 4x6 double matrix.
+    C4_Datatype data_type;
+    int ierr = create_vector_type<double>(3, 2, 4, data_type);
+
+    if (ierr == C4_SUCCESS)
+    {
+        ut.passes("created vector type successfully");
+    }
+    else
+    {
+        ut.failure("did NOT create vector type successfully");
+    }
+
+    // try sending a couple
+
+    double array[4*6];
+
+    unsigned proc = node();
+    
+    for (unsigned j=0; j<6; ++j)
+    {
+        for (unsigned i=0; i<4; ++i)
+        {
+            if (proc>0)
+                array[i+4*j] = 0;
+            else
+                array[i+4*j] = 10*i + j;
+        }
+    }
+
+    unsigned nproc = nodes();
+    if (proc==0)
+    {
+        for (unsigned p=1; p<nproc; ++p)
+        {
+            send(array+(1+4*2), 1, p, data_type);
+        }
+    }
+    else
+    {
+        receive(array+(1+4*2), 1, 0, data_type);
+        for (unsigned j=0; j<6; ++j)
+        {
+            for (unsigned i=0; i<4; ++i)
+            {
+                if (i>=1 && i<=2 && j>=2 && j<=4)
+                {
+                    if (array[i+4*j] != 10*i + j)
+                    {
+                        ut.failure("did NOT transmit correct subarray");
+                        return;
+                    }
+                }
+                else
+                {
+                    if (array[i+4*j] != 0)
+                    {
+                        ut.failure("did NOT transmit correct subarray");
+                        return;
+                    }
+                }
+            }
+        }
+        ut.passes("transmitted correct subarray");
+    }
+
+    type_free(data_type);
+}
+
+//---------------------------------------------------------------------------//
+
+int main(int argc, char *argv[])
+{
+    rtt_c4::ParallelUnitTest ut(argc, argv, rtt_dsxx::release);
+    try
+    {
+        // Unit tests
+        test_simple(ut);
+    }
+    catch (std::exception &err)
+    {
+        std::cout << "ERROR: While testing tstVector_Type, " 
+                  << err.what() << std::endl;
+        ut.numFails++;
+    }
+    catch( ... )
+    {
+        std::cout << "ERROR: While testing tstVector_Type, " 
+                  << "An unknown exception was thrown." << std::endl;
+        ut.numFails++;
+    }
+    return ut.numFails;
+}   
+
+//---------------------------------------------------------------------------//
+//                        end of tstVector_Type.cc
+//---------------------------------------------------------------------------//
