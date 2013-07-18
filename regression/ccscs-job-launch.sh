4f70e1bbc5f0a7f017dcfd0d1c2695dac0df4ccb
kellyt <kellyt@2e4270bf-7bdd-41b3-a9ef-8cbd4d332402>
2013-07-18 19:11:48 +0000

    + Fix a typo in the ccs regression script. + Reduce the sleep command from 10 to 5 minutes between blocking   checks.

--- regression/ccscs-job-launch.sh
+++ regression/ccscs-job-launch.sh
@@ -68,7 +68,7 @@ echo "   ${subproj}: dep_jobids = ${dep_jobids}"
 for jobid in ${dep_jobids}; do
     while [ `ps --no-headers -u ${USER} -o pid | grep ${jobid} | wc -l` -gt 0 ]; do
        echo "   ${subproj}: waiting for jobid = $jobid to finish."
-       sleep 10m
+       sleep 5m
     done
 done
 
