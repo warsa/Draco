b9be66e43fa9a241509fa6fa8843a27603193019
kellyt <kellyt@2e4270bf-7bdd-41b3-a9ef-8cbd4d332402>
2013-07-23 14:46:04 +0000

    + Provide a check for the C99 restrict keyword availability.  Save   result to ds++/config.h.

--- config/platform_checks.cmake
+++ config/platform_checks.cmake
@@ -125,23 +125,6 @@ macro( query_have_sys_headers )
 endmacro()
 
 ##---------------------------------------------------------------------------##
-## Determine Word Types
-##
-## Query sizes of PODTs.
-##---------------------------------------------------------------------------##
-
-# macro( determine_word_types )
-#    include(CheckTypeSize)
-#    check_type_size( "int"       SIZEOF_INT )
-#    check_type_size( "long"      SIZEOF_LONG )
-#    check_type_size( "long long" SIZEOF_LONG_LONG )
-   
-#    check_type_size( "float"       SIZEOF_FLOAT )
-#    check_type_size( "double"      SIZEOF_DOUBLE )
-#    check_type_size( "long double" SIZEOF_LONG_DOUBLE )
-# endmacro()
-
-##---------------------------------------------------------------------------##
 ## Check 8-byte int type
 ##
 ## For some systems, provide special compile flags to support 8-byte integers
@@ -158,42 +141,19 @@ macro(check_eight_byte_int_type)
       message( "Checking for 8-byte integer type... long - no mods needed." )
    else()
       message( FATAL_ERROR "need to patch up this part of the build system." )
-# See ac_dracotests.m4
-#      if( ${CMAKE_SYSTEM} MATCHES AIX )
-#         if with_cxx = ibm and vendor_mpi and strict ansi, then add
-#      -qlanglvl=extended -qlonglong
-#         fi
-#      endif()
    endif()
 endmacro()
 
 ##---------------------------------------------------------------------------##
-## Wedgehog types
+## Detect support for the C99 restrict keyword
 ##---------------------------------------------------------------------------##
+macro( query_have_restrict_keyword )
+   
+   check_c_source_compiles(
+      "int test (void *restrict x); int main (void) {return 0;}"
+      HAVE_RESTRICT) 
 
-# macro( wedgehog_types )
-#    if( "${HOST_INT_SIZE}" STREQUAL "${SIZEOF_INT}" )
-#       set( HOST_INT "int" )
-#    elseif( "${HOST_INT_SIZE}" STREQUAL "${SIZEOF_LONG}" )
-#       set( HOST_INT "long" )
-#    elseif( "${HOST_INT_SIZE}" STREQUAL "${SIZEOF_LONG_LONG}" )
-#       set( HOST_INT "long long" )
-#    endif()
-
-#    if( "${EIGHT_BYTE_INT_SIZE}" STREQUAL "${SIZEOF_INT}" )
-#       set( EIGHT_BYTE_INT "int" )
-#    elseif( "${EIGHT_BYTE_INT_SIZE}" STREQUAL "${SIZEOF_LONG}" )
-#       set( EIGHT_BYTE_INT "long" )
-#    elseif( "${EIGHT_BYTE_INT_SIZE}" STREQUAL "${SIZEOF_LONG_LONG}" )
-#       set( EIGHT_BYTE_INT "long long" )
-#    endif()
-
-#    if( "${HOST_FLOAT_SIZE}" STREQUAL "${SIZEOF_FLOAT}" )
-#       set( HOST_FLOAT "float" )
-#    elseif( "${HOST_FLOAT_SIZE}" STREQUAL "${SIZEOF_DOUBLE}" )
-#       set( HOST_FLOAT "double" )
-#    endif()
-# endmacro()
+endmacro()
 
 ##---------------------------------------------------------------------------##
 ## Sample platform checks
