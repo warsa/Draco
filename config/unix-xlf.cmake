a4f5ee2b33dd1135e175e6a594326a24ffb9ffd4
kellyt <kellyt@2e4270bf-7bdd-41b3-a9ef-8cbd4d332402>
2014-01-08 16:35:18 +0000

    + While preparing to release Draco, it was observed that the FortranChecks C++   unit test did not compile correctly on SQ.  The root cause was the inclusion   of a superfluous Fortran library (xlomp_ser) added to the link line   automatically by CMake.  To resolve this issue, unix-xlf.cmake was updated   to forcibly remove this library from CMAKE_Fortran_IMPLICIT_LINK_LIBRARIES. + Minor cleanup was applied to related files. refs #307 @4h

--- config/unix-xlf.cmake
+++ config/unix-xlf.cmake
@@ -3,7 +3,7 @@
 # author Gabriel Rockefeller
 # date   2012 Nov 1
 # brief  Establish flags for Unix - IBM XL Fortran
-# note   Copyright (C) 2012 Los Alamos National Security, LLC.
+# note   Copyright (C) 2012-2013 Los Alamos National Security, LLC.
 #        All rights reserved.
 #------------------------------------------------------------------------------#
 # $Id$
@@ -42,15 +42,59 @@ set( CMAKE_Fortran_COMPILER_FLAVOR "XL" )
 #                 eXtension (QPX) units.
 # -qsmp=omp       Enable parallelization using OpenMP pragmas.
 
-set( CMAKE_Fortran_FLAGS    "-qlanglvl=2003std -qinfo=all -qflag=i:w -qarch=auto" )
-if( USE_OPENMP )
-   set( CMAKE_Fortran_FLAGS "${CMAKE_Fortran_FLAGS} -qsmp=omp" )
+if( NOT CXX_FLAGS_INITIALIZED )
+   set( CXX_FLAGS_INITIALIZED "yes" CACHE INTERNAL "using draco settings." )
+
+   set( CMAKE_Fortran_FLAGS                "-qlanglvl=2003std -qinfo=all -qflag=i:w -qarch=auto" )
+   set( CMAKE_Fortran_FLAGS_DEBUG          "-g -O0 -qnosmp -qcheck" )
+   set( CMAKE_Fortran_FLAGS_RELEASE        "-O3 -qhot=novector -qsimd=auto -qstrict=nans:operationprecision" )
+   set( CMAKE_Fortran_FLAGS_MINSIZEREL     "${CMAKE_Fortran_FLAGS_RELEASE}" )
+   set( CMAKE_Fortran_FLAGS_RELWITHDEBINFO "-g -O3 -qhot=novector -qsimd=auto -qstrict=nans:operationprecision" )
+
 endif()
 
-set( CMAKE_Fortran_FLAGS_DEBUG          "-g -O0 -qnosmp -qcheck" )
-SET( CMAKE_Fortran_FLAGS_RELEASE        "-O3 -qhot=novector -qsimd=auto -qstrict=nans:operationprecision" )
-SET( CMAKE_Fortran_FLAGS_MINSIZEREL     "${CMAKE_Fortran_FLAGS_RELEASE}" )
-SET( CMAKE_Fortran_FLAGS_RELWITHDEBINFO "-g -O3 -qhot=novector -qsimd=auto -qstrict=nans:operationprecision" )
+##---------------------------------------------------------------------------##
+# Ensure cache values always match current selection
+##---------------------------------------------------------------------------##
+set( CMAKE_Fortran_FLAGS                "${CMAKE_Fortran_FLAGS}"                CACHE STRING "compiler flags" FORCE )
+set( CMAKE_Fortran_FLAGS_DEBUG          "${CMAKE_Fortran_FLAGS_DEBUG}"          CACHE STRING "compiler flags" FORCE ) 
+set( CMAKE_Fortran_FLAGS_RELEASE        "${CMAKE_Fortran_FLAGS_RELEASE}"        CACHE STRING "compiler flags" FORCE )
+set( CMAKE_Fortran_FLAGS_MINSIZEREL     "${CMAKE_Fortran_FLAGS_MINSIZEREL}"     CACHE STRING "compiler flags" FORCE )
+set( CMAKE_Fortran_FLAGS_RELWITHDEBINFO "${CMAKE_Fortran_FLAGS_RELWITHDEBINFO}" CACHE STRING "compiler flags" FORCE )
+
+toggle_compiler_flag( USE_OPENMP "-qsmp=omp" "Fortran" "")
+
+# ----------------------------------------------------------------------------
+# Helper macro to fix the value of
+# CMAKE_Fortran_IMPLICIT_LINK_LIBRARIES when compiling on SQ with
+# xlc/xlf.  For some reason CMake is adding xlomp_ser to the link line
+# when a C++ main is built by linking to F90 libraries.  This library
+# should not be on For some reason CMake is adding xlomp_ser to the
+# link line when a C++ main the link line and actually causes the link
+# to fail on SQ.
+# ----------------------------------------------------------------------------
+function( remove_lib_from_link lib_for_removal )
+
+   foreach( lib ${CMAKE_Fortran_IMPLICIT_LINK_LIBRARIES} )
+      if( ${lib} MATCHES ${lib_for_removal} )
+         message("Removing ${lib_for_removal} from CMAKE_Fortran_IMPLICIT_LINK_LIBRARIES")
+         # do not add ${lib_for_removal} to the list ${tmp}.
+      else()
+         # Add a libraries except for the above match to the new list.
+         list( APPEND tmp ${lib} )
+      endif()
+   endforeach()
+
+   # Ensure that the value is saved to CMakeCache.txt
+   set( CMAKE_Fortran_IMPLICIT_LINK_LIBRARIES "${tmp}" CACHE STRING
+      "Extra Fortran libraries needed when linking to Fortral library from C++ target"
+      FORCE )
+
+endfunction()
+
+# When building on SQ with XLC/XLF90, ensure that libxlomp_ser is not
+# on the link line
+remove_lib_from_link( "xlomp_ser" )
 
 #------------------------------------------------------------------------------#
 # End config/unix-xlf.cmake
