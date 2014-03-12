46a705d25ed38959439f0385184736a0f5221efa
kellyt <kellyt@2e4270bf-7bdd-41b3-a9ef-8cbd4d332402>
2014-03-12 20:38:28 +0000

    + push/pull repository scripts that use transfer 2.0 should now be working.

--- regression/pull_repositories_xf.sh
+++ regression/pull_repositories_xf.sh
@@ -11,23 +11,14 @@
 #    jayenne.repo    jayenne.hotcopy.tar   jayenne.hotcopy
 #    capsaicin.repo  capsaicin.hotcopy.tar capsaicin.hotcopy
 # 2. SVN repositories live at /usr/projects/draco/svn
-# 3. Kerberos keytab files is at $HOME/.ssh/cron.keytab and is signed
-#    with principal $USER@lanl.gov
+# 3. Kerberos keytab files is at $HOME/.ssh/xfkeytab and is signed
+#    with principal transfer/${USER}push@lanl.gov
 #------------------------------------------------------------------------------#
 # How to generate keytab files:
-#
-# /usr/kerberos/sbin/kadmin -p ${USER}@lanl.gov
-# > ktadd -k $HOME/.ssh/cron.keytab -p ${USER}@lanl.gov
-# > quit
-# chmod 600 $HOME/.ssh/cron.keytab
-
-# Enable kerberos credentials via keytab authentication:
-#
-# kinit -f -l 8h -kt $HOME/.ssh/cron.keytab ${USER}@lanl.gov
+# - See notes in push_repositories_xf.sh or the Draco Wiki.
+# Obtain credentials for pull:
+#   kinit -f -l 8h -kt $HOME/.ssh/xfkeytab transfer/${USER}push@lanl.gov
 #------------------------------------------------------------------------------#
-
-#STATUS=/usr/local/bin/status
-#PULL=/usr/local/bin/pull
 
 # dry_run=1
 
@@ -77,40 +68,40 @@ function xfpull()
 
 unpack_repo() {
    pkg=$1
-   # echo "Remove old files..."
-   # if test -f ${pkg}.hotcopy.tar; then
-   #    run "rm -f ${pkg}.hotcopy.tar"
-   # fi
-   # if test -d ${pkg}.hotcopy; then
-   #    run "rm -rf ${pkg}.hotcopy"
-   # fi
-   # if test -d ${pkg}; then
-   #   if test -d ${pkg}.old; then
-   #      run "rm -rf ${pkg}.old"
-   #   fi
-   # fi
+   echo "Remove old files..."
+   if test -f ${pkg}.hotcopy.tar; then
+      run "rm -f ${pkg}.hotcopy.tar"
+   fi
+   if test -d ${pkg}.hotcopy; then
+      run "rm -rf ${pkg}.hotcopy"
+   fi
+   if test -d ${pkg}; then
+     if test -d ${pkg}.old; then
+        run "rm -rf ${pkg}.old"
+     fi
+   fi
    
    echo "Unpacking SVN repository for $pkg ..."
    run "xfpull ${pkg}.hotcopy.tar"
-   #run "tar -xvf ${pkg}.hotcopy.tar"
-   #if test -d ${pkg}.hotcopy; then
-   #  run "mv ${pkg} ${pkg}.old"
-   #  run "mv ${pkg}.hotcopy ${pkg}"
-   #fi
+   run "tar -xvf ${pkg}.hotcopy.tar"
+   if test -d ${pkg}.hotcopy; then
+      run "mv ${pkg} ${pkg}.old"
+      run "mv ${pkg}.hotcopy ${pkg}"
+   fi
    echo " "
 }
 
 # working directory
-# start_dir=`pwd`
-# work_dir=/usr/projects/draco/svn
-# if test -d $work_dir; then
-#    cd $work_dir
-# else
-#    die "could not cd to $work_dir"
-# fi
+start_dir=`pwd`
+work_dir=/usr/projects/draco/svn
+if test -d $work_dir; then
+   run "cd $work_dir"
+else
+   die "could not cd to $work_dir"
+fi
 
 # Ensure we have a kerberos ticket
-run "kinit -f -l 1h -kt $HOME/.ssh/xfkeytab transfer/kellytpush@lanl.gov"
+run "kinit -f -l 1h -kt $HOME/.ssh/xfkeytab transfer/${USER}push@lanl.gov"
 
 # Ask Mercury if there are any items available for pulling from Yellow
 possible_items_to_pull=`ssh red@transfer.lanl.gov myfiles | awk '{print $2}'`
@@ -134,6 +125,6 @@ if test ${jayenne_ready} = 1; then unpack_repo "jayenne"; fi
 if test ${capsaicin_ready} = 1; then unpack_repo "capsaicin"; fi
 
 # Update permisssions as needed
-# run "cd ${work_dir}/.."
-# run "chgrp -R draco svn"
-# run "chmod -R g+rwX,o-rwX svn"
+run "cd ${work_dir}/.."
+run "chgrp -R draco svn"
+run "chmod -R g+rwX,o-rwX svn"
--- regression/push_repositories_xf2.sh
+++ regression/push_repositories_xf2.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
 
+# Q: How do I create a keytab that works with transfer 2.0
+# A: See
+# https://rtt.lanl.gov/redmine/projects/draco/wiki/Kelly_Thompson#Generating-keytab-file-that-works-with-transfer-20
+# or see the comments at the end of this file.
+
 # Obtain kerberos authentication via keytab
-kinit -f -l 1h -kt $HOME/.ssh/xfkeytab transfer/kellytpush@lanl.gov
+kinit -f -l 1h -kt $HOME/.ssh/xfkeytab transfer/${USER}push@lanl.gov
 
 # Helpful functions:
 die () { echo "FATAL ERROR: $1"; exit 1;}
@@ -26,22 +31,105 @@ repos="draco jayenne capsaicin"
 #
 for repo in $repos; do
    # Remove old hotcopy files/directories.
-   # if test -f ${repo}.hotcopy.tar; then
-   #    run "rm -f ${repo}.hotcopy.tar"
-   # fi
-   # if test -d ${repo}.hotcopy; then
-   #    run "rm -rf ${repo}.hotcopy"
-   # fi
-   # # Generate a repo hotcopy
-   # run "svnadmin hotcopy $repo ${repo}.hotcopy"
-   # # Tar it up and push via mercury.
-   # run "tar -cvf ${repo}.hotcopy.tar ${repo}.hotcopy"
-   
-   #run "${PUSH} push2r --quiet ${repo}.hotcopy.tar"
+   if test -f ${repo}.hotcopy.tar; then
+      run "rm -f ${repo}.hotcopy.tar"
+   fi
+   if test -d ${repo}.hotcopy; then
+      run "rm -rf ${repo}.hotcopy"
+   fi
+   # Generate a repo hotcopy
+   run "svnadmin hotcopy $repo ${repo}.hotcopy"
+
+   # Tar it up and push via mercury.
+   run "tar -cvf ${repo}.hotcopy.tar ${repo}.hotcopy"
+
+   # Transfer the file via transfer.lanl.gov
    run "scp ${repo}.hotcopy.tar red@transfer.lanl.gov:"
 
    # Ensure the new files have group rwX permissions.
-   # run "chgrp -R draco ${repo}.hotcopy.tar ${repo}.hotcopy"
-   # run "chmod -R g+rwX,o=g-w ${repo}.hotcopy.tar ${repo}.hotcopy"
+   run "chgrp -R draco ${repo}.hotcopy.tar ${repo}.hotcopy"
+   run "chmod -R g+rwX,o=g-w ${repo}.hotcopy.tar ${repo}.hotcopy"
 done
 
+#------------------------------------------------------------------------------#
+# Notes on using Transfer 2.0 (copied from the Draco wiki):
+
+# * See http://transfer.lanl.gov.
+
+# <pre>
+# % kinit -kt ~/.ssh/xfkeytab transfer/${USER}push@lanl.gov
+# % ssh yellow@transfer.lanl.gov myfiles
+# % scp /ccs/codes/radtran/svn/draco.kt.tar red@transfer.lanl.gov:
+# </pre>
+
+# * Using the transfer.sh script:
+
+# <pre>
+# draco/tools/transfer.sh push2r --recipients=kgt@lanl.gov -q `pwd`/<file>
+# draco/tools/transfer.sh pullname <file>
+# </pre>
+
+# h3. Generating keytab file that works with transfer 2.0.
+
+# * Part1: Obtain a business account via http://register.lanl.gov
+# ** Under "Business Accounts Menu", click "Create a Business Account"
+# ** Enter a name, (throughout this example, I will use <your_account>), then [Next]. 
+# ** Enter Account Description, then [Next]
+# *** [Confirm]
+# ** Now, click "Manage Kerberos Principals"
+# ** In the box, enter "transfer/" (without the quotes!) and [Create]. 
+# ** At the "Are you sure..." prompt, [Create]
+# ** You should have gotten a message that says,
+# <pre>
+#    Kerberos principal transfer/<your_account> was successfully Created.
+# </pre>
+
+# * Part 2: Register the principals
+# ** Go to https://transfer.lanl.gov/principals
+# ** Under "Add a New Principal", after the “transfer/”, enter your account name in the box:
+# <pre>
+#     transfer/<your_account>, click [Enroll Yellow Principal]
+# </pre>
+# ** If everything is correct, the four checkboxes below will get a green [Check]. If not, fix the first one that failed and try again. Repeat until success.
+
+# * Part 3: Create the file
+# ** Now, on the machine that you are going to use transfers*:
+# ** *Note: Use ZNumber, not moniker here:*
+# <pre>
+#   % kadmin -p <ZNumber>@lanl.gov
+#   Authenticating as principal <ZNumber>@lanl.gov with password.
+#   Password for <ZNumber>@lanl.gov:  <cryptocard password>
+# </pre>
+# ** Now, at the kadmin: prompt, use the principal that you created, and append @lanl.gov (note in the xst command, “transfer_keytab” is the name of the file that will be created)
+# <pre>
+#    kadmin:   xst -k transfer_keytab transfer/<your_account>@lanl.gov 
+# Entry for principal transfer/<your_account>@lanl.gov with kvno 2, encryption type des-cbc-crc added to keytab WRFILE:transfer_keytab.
+# Entry for principal transfer/<your_account>@lanl.gov with kvno 2, encryption type arcfour-hmac added to keytab WRFILE:transfer_keytab.
+# Entry for principal transfer/<your_account>@lanl.gov with kvno 2, encryption type des3-cbc-sha1 added to keytab WRFILE:transfer_keytab.
+# Entry for principal transfer/<your_account>@lanl.gov with kvno 2, encryption type aes128-cts-hmac-sha1-96 added to keytab
+# WRFILE:transfer_keytab.
+# Entry for principal transfer/<your_account>@lanl.gov with kvno 2, encryption type aes256-cts-hmac-sha1-96 added to keytab
+# WRFILE:transfer_keytab.
+#    kadmin: quit
+# </pre>
+
+# * Part 4: Test the file
+# ** [first example, without the extracted key, no credentials]
+# <pre>
+#   % kdestroy 
+#   kdestroy: No credentials cache found while destroying cache
+
+#   % ssh yellow@transfer.lanl.gov myfiles
+#   yellow@transfer.lanl.gov's password:  <Ctl-C>
+# </pre>
+# ** [it did not work; now, use the keytab file]
+# <pre>
+#   % kinit -kt ./transfer_keytab transfer/<your_account>@lanl.gov
+
+#   % ssh yellow@transfer.lanl.gov myfiles
+#   ! No files found for zno 089990
+# </pre>
+
+# * Part 5: Request principal migration to Red
+# ** https://transfer.lanl.gov/principal_request
+# ** Now, log onto the red web page, https://transfer.lanl.gov/principals to register the same identity.
