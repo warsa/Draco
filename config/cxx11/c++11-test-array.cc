4b84a78c7077a062e9f3501f9d1f9c25dd5200e2
kellyt <kellyt@2e4270bf-7bdd-41b3-a9ef-8cbd4d332402>
2014-03-28 15:55:02 +0000

    + Update the test for std::array to use syntax that is compatible with both   C++11 and C++14.  Intel/14.0.2 was tripping on this case.

--- config/cxx11/c++11-test-array.cc
+++ config/cxx11/c++11-test-array.cc
@@ -4,6 +4,6 @@ int main()
 {
     std::array<double,3> foo{
         { 0.3247333291470, 0.3224333221471, 0.3324333522912 } };
-    std::array<int,4> bar{1,2,3,4};
+    std::array<int,4> bar{{1,2,3,4}};
     return 0;
 }
