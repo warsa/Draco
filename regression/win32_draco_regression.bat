c83b098a1230787349c5b97db616de2614c56ca9
kellyt <kellyt@2e4270bf-7bdd-41b3-a9ef-8cbd4d332402>
2014-07-23 15:47:14 +0000

    + This updates moves the win32 nightly regression from VS 2012 to 2013.

--- regression/win32_draco_regression.bat
+++ regression/win32_draco_regression.bat
@@ -9,13 +9,6 @@ rem In Task Scheduler, create to actions:
 rem 1. d:\cdash\draco\regression\update_regression_dir.bat
 rem 2. c:\windows\system32\cmd.exe /k ""d:\cdash\draco\regression\win32_draco_regression.bat"" x86
 
-rem set VisualStudioVersion=11.0
-rem set vsloc="C:\Progra~2\Microsoft Visual Studio %VisualStudioVersion%\VC\bin"
-rem set vsenvbat=vcvars32.bat
-set VisualStudioVersion=12.0
-set vsloc="C:\Progra~2\Microsoft Visual Studio %VisualStudioVersion%\Common7\Tools"
-set vsenvbat=VsDevCmd.bat
-
 @echo off
 if "%1" == "" goto x86
 if not "%2" == "" goto usage
@@ -30,8 +23,8 @@ goto usage
 
 :x86
 if not exist "%vsloc%\%vsenvbat%" goto missing
-call "%vsloc%\%vsenvbat%"
-goto :cdash
+call "c:\Program Files (x86)\Microsoft Visual Studio 12.0\Common7\Tools\VsDevCmd.bat" x86
+goto :vendorsetup
 
 :usage
 echo Error in script usage. The correct usage is:
@@ -47,25 +40,27 @@ echo The specified configuration type is missing.  The tools for the
 echo configuration might not be installed.
 goto :eof
 
-:cdash
+:vendorsetup
+set VENDOR_DIR=d:\work\vendors
+set GSL_INC_DIR=%VENDOR_DIR%\gsl-1.16\include
+set GSL_LIB_DIR=%VENDOR_DIR%\gsl-1.16\lib
+set QTDIR=c:/Qt/5.3/msvc2013
 set SVN_SSH=c:\\Program Files\\TortoiseSVN\\bin\\TortoisePlink.exe
+
+:cdash
 rem set dashboard_type=Experimental
 set dashboard_type=Nightly
 set base_dir=d:\cdash
-set build_type=Debug
 set comp=cl
 set script_dir=d:\cdash\draco\regression
 set script_name=Draco_Win32.cmake
 set ctestparts=Configure,Build,Test,Submit
 
 set subproj=draco
-set work_dir=%base_dir%\%subproj%\%dashboard_type%_%comp%\%build_type%
 
-:vendorsetup
-set VENDOR_DIR=d:\work\vendors
-set GSL_INC_DIR=%VENDOR_DIR%\gsl-1.16\include
-set GSL_LIB_DIR=%VENDOR_DIR%\gsl-1.16\lib
-set QTDIR=c:/Qt/5.3/msvc2012_opengl
+:cdashdebug
+set build_type=Debug
+set work_dir=%base_dir%\%subproj%\%dashboard_type%_%comp%\%build_type%
 
 rem print some information
 echo Environment:
@@ -85,10 +80,30 @@ if not exist %work_dir%\source mkdir source
 if not exist %work_dir%\target mkdir target
 
 rem run the ctest script
-rem d:\cdash\draco\Experimental_cl\Debug>ctest -VV -S d:\cdash\draco\regression\Draco_Win32.cmake,Experimental,Debug,Configure,Build,Test,Submit > my.log 2>&1
-echo ctest -VV -S %script_dir%\%script_name%,%dashboard_type%,%build_type%,%ctestparts%
-ctest -VV -S %script_dir%\%script_name%,%dashboard_type%,%build_type%,%ctestparts% > %base_dir%\logs\draco-cbts.log
 
+echo ctest -VV -S %script_dir%\%script_name%,%dashboard_type%,%build_type%,%ctestparts%
+ctest -VV -S %script_dir%\%script_name%,%dashboard_type%,%build_type%,%ctestparts% > %base_dir%\logs\draco-%build_type%-cbts.log
+
+:cdashrelease
+set build_type=Release
+set work_dir=%base_dir%\%subproj%\%dashboard_type%_%comp%\%build_type%
+
+rem navigate to the workdir
+if not exist %work_dir% mkdir %work_dir%
+cd /d %work_dir%
+
+rem clear the build directory (need to do this here to avoid a hang).
+rem if exist %work_dir%\build rmdir /s /q build
+if not exist %work_dir%\build mkdir build
+if not exist %work_dir%\source mkdir source
+if not exist %work_dir%\target mkdir target
+
+rem run the ctest script
+
+echo ctest -VV -S %script_dir%\%script_name%,%dashboard_type%,%build_type%,%ctestparts%
+ctest -VV -S %script_dir%\%script_name%,%dashboard_type%,%build_type%,%ctestparts% > %base_dir%\logs\draco-%build_type%-cbts.log
+
+:done
 echo You need to remove -k from script launch to let this window close automatically.
 
 
