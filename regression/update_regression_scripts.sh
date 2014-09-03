a5d679dd3960c36c62f8261f5557af37c0c0a4e8
kellyt <kellyt@2e4270bf-7bdd-41b3-a9ef-8cbd4d332402>
2014-09-03 22:10:01 +0000

    + Begin standing up a regression system on Darwin.  For now, provide a crontab   and a script that updates the regression scripts.

--- regression/update_regression_scripts.sh
+++ regression/update_regression_scripts.sh
@@ -1,21 +1,32 @@
 #!/bin/bash
 
-# Ensure that the permissions are correct
 umask 0002
-SVN=/ccs/codes/radtran/vendors/subversion-1.8.5/bin/svn
+
+target="`uname -n | sed -e s/[.].*//`"
+arch=`uname -m`
+
+# Ensure that the permissions are correct
+case ${target} in
+darwin* | cn[0-9]*)
+   SVN=/projects/opt/subversion/1.7.14/bin/svn
+   REGDIR=/projects/opt/draco/regress
+   ;;
+*)
+   SVN=/ccs/codes/radtran/vendors/subversion-1.8.5/bin/svn
+   REGDIR=/home/regress
+   ;;
+esac
 
 # Helper function
 run () {
     echo $1
-    if ! [ $dry_run ]; then
-       eval $1
-    fi
+    if ! [ $dry_run ]; then eval $1; fi
 }
 
-run "cd /home/regress/draco/config; ${SVN} update"
-run "cd /home/regress/draco/regression; ${SVN} update"
-run "cd /home/regress/draco/environment; ${SVN} update"
-run "cd /home/regress/jayenne/regression; ${SVN} update"
-run "cd /home/regress/capsaicin/scripts; ${SVN} update"
-run "cd /home/regress/asterisk/regression; ${SVN} update"
+run "cd ${REGDIR}/draco/config; ${SVN} update"
+run "cd ${REGDIR}/draco/regression; ${SVN} update"
+run "cd ${REGDIR}/draco/environment; ${SVN} update"
+run "cd ${REGDIR}/jayenne/regression; ${SVN} update"
+run "cd ${REGDIR}/capsaicin/scripts; ${SVN} update"
+run "cd ${REGDIR}/asterisk/regression; ${SVN} update"
 
