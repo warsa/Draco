8c1b6653b6895ff11b8a64b8a34e32dd97358e38
kellyt <kellyt@2e4270bf-7bdd-41b3-a9ef-8cbd4d332402>
2015-05-15 16:10:46 +0000

    + Forgot to remove the old calls to soft_equiv.  Using the new signatures now.

--- src/ds++/test/tstSoft_Equiv.cc
+++ src/ds++/test/tstSoft_Equiv.cc
@@ -266,7 +266,6 @@ void test_vector_specialization(rtt_dsxx::ScalarUnitTest & ut)
         // 1-d vector comparison.
         std::vector<double> v(27,epsilon);
         std::vector<double> r(27,epsilon);
-        if( ! soft_equiv_deep<1,double>().equiv(v.begin(),v.end(),r.begin(),r.end()) ) ITFAILS;
         if( ! soft_equiv(v,r) ) ITFAILS;
     }
     {
@@ -278,7 +277,6 @@ void test_vector_specialization(rtt_dsxx::ScalarUnitTest & ut)
             v[i] = std::vector<double>(i*2,epsilon);
             r[i] = std::vector<double>(i*2,epsilon);
         }
-        if( ! soft_equiv_deep<2,double>().equiv(v.begin(),v.end(),r.begin(),r.end()) ) ITFAILS;
         if( ! soft_equiv(v,r) ) ITFAILS;
     }
     {
@@ -295,7 +293,6 @@ void test_vector_specialization(rtt_dsxx::ScalarUnitTest & ut)
                 r[i][j] = std::vector<double>(j*2,epsilon);
             }
         }
-        if( ! soft_equiv_deep<3,double>().equiv(v.begin(),v.end(),r.begin(),r.end()) ) ITFAILS;
         if( ! soft_equiv(v,r) ) ITFAILS;
     }
     {
@@ -303,14 +300,12 @@ void test_vector_specialization(rtt_dsxx::ScalarUnitTest & ut)
         std::vector<double> v(27,42.42);
         std::vector<double> r(27,42.42);
         r[5] = 42.44; // mismatch value
-        if( soft_equiv_deep<1,double>().equiv(v.begin(),v.end(),r.begin(),r.end()) ) ITFAILS;
         if( soft_equiv(v,r) ) ITFAILS;
     }
     {
         // expect a failure for mismatched size.
         std::vector<double> v(27,42.42);
         std::vector<double> r(7, 42.42);
-        if( soft_equiv_deep<1,double>().equiv(v.begin(),v.end(),r.begin(),r.end()) )
         if( soft_equiv(v,r) )
             FAILMSG("Comparing different size vectors should not pass!");
         else
