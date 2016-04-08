2e39560974973ce4874f7eeb4eac89036a89a0b1
kellyt <kellyt@2e4270bf-7bdd-41b3-a9ef-8cbd4d332402>
2016-04-08 16:42:19 +0000

    + Remove a debug print that was accidentally left in a previous commit.

--- config/unix-clang.cmake
+++ config/unix-clang.cmake
@@ -1,10 +1,8 @@
 #-----------------------------*-cmake-*----------------------------------------#
 # file   config/unix-clang.cmake
 # brief  Establish flags for Unix clang
-# note   Copyright (C) 2010-2013 Los Alamos National Security, LLC.
+# note   Copyright (C) 2010-2016 Los Alamos National Security, LLC.
 #        All rights reserved.
-#------------------------------------------------------------------------------#
-# $Id: unix-g++.cmake 6798 2012-10-09 21:45:34Z kellyt $
 #------------------------------------------------------------------------------#
 
 #
@@ -22,7 +20,7 @@ if( NOT CXX_FLAGS_INITIALIZED )
 
    set( CMAKE_C_FLAGS                "-Wcast-align -Wpointer-arith -Wall -Wno-long-long -pedantic" )
    if (NOT ${CMAKE_GENERATOR} MATCHES Xcode AND HAS_MARCH_NATIVE)
-      set( CMAKE_C_FLAGS                "${CMAKE_C_FLAGS} -march=native" )
+      set( CMAKE_C_FLAGS             "${CMAKE_C_FLAGS} -march=native" )
    endif()
    set( CMAKE_C_FLAGS_DEBUG          "-g -fno-inline -O0 -Wextra -DDEBUG")
    set( CMAKE_C_FLAGS_RELEASE        "-O3 -funroll-loops -DNDEBUG" )
@@ -57,21 +55,8 @@ set( CMAKE_CXX_FLAGS_RELWITHDEBINFO "${CMAKE_CXX_FLAGS_RELWITHDEBINFO}" CACHE ST
 
 # Toggle for C++11 support
 if( OpenMP_C_FLAGS )
-  message("toggle_compiler_flag( OPENMP_FOUND \"${OpenMP_C_FLAGS}\" \"C;CXX;EXE_LINKER\" \"\" )")
   toggle_compiler_flag( OPENMP_FOUND "${OpenMP_C_FLAGS}" "C;CXX;EXE_LINKER" "" )
 endif()
-
-#============================================================
-# Notes for bulding clang 3.5.0
-#============================================================
-# Attempt to install clang/llvm 3.5.0 on ccscs7
-# - http://linuxdeveloper.blogspot.com/2012/12/building-llvm-32-from-source.html
-# - In the source tree, create this symlink: llvm-3.5.0.src/tools/clang -> ../../cfe-3.5.0.src
-# - Used modules for cmake, gcc/4.8.1.
-# - ../llvm/configure \
-#   --prefix=/ccs/codes/radtran/vendors/llvm-3.5.0 --enable-shared \
-#   --with-gcc-toolchain=/ccs/codes/radtran/vendors/gcc-4.8.1 \
-# - make -j 8 [install]
 
 #------------------------------------------------------------------------------#
 # End config/unix-clang.cmake
