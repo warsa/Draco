258c2a6880f561e9c7f0b79afa6a3aaee61fb0a2
Kendra P. Keady <keadyk@lanl.gov>
2017-02-10 13:01:35 -0700

    Update to Compton_NWA
    + Add a constructor call for on the fly multigroup opacity builds (slow and not recommended)
    + Add interpolate() method for electron temperature

--- src/compton/Compton_NWA.cc
+++ src/compton/Compton_NWA.cc
@@ -1,10 +1,29 @@
+//----------------------------------*-C++-*----------------------------------//
+/*!
+ * \file   compton/Compton_NWA.hh
+ * \author Kendra Keady
+ * \date   Mon Apr  2 14:14:29 2001
+ * \brief  Implementation file for compton NWA interface -- linked against library
+ * \note   Copyright (C) 2017 Los Alamos National Security, LLC.
+ *         All rights reserved.
+ */
+//---------------------------------------------------------------------------//
+// $Id$
+//---------------------------------------------------------------------------//
+
 #include "Compton_NWA.hh"
 #include "compton_file.hh"
 #include "multigroup_compton_data.hh"
+#include "multigroup_data_types.hh"
 #include "multigroup_lib_builder.hh"
+#include "ds++/Assert.hh"
 
 namespace rtt_compton {
 
+// Constructor for an existing multigroup libfile.
+// This calls NWA methods to read the data file and store everything in a
+// Compton data object, a smart pointer to which is then passed to (and held by)
+// the NWA etemp_interp class.
 Compton_NWA::Compton_NWA(const std::string &filehandle) {
 
   // Make a compton file object
@@ -12,5 +31,66 @@ Compton_NWA::Compton_NWA(const std::string &filehandle) {
 
   std::shared_ptr<multigroup_compton_data> Cdata =
       Cfile.read_mg_csk_data(filehandle);
+
+  Ensure(Cdata);
+
+  ei.reset(new etemp_interp(Cdata));
+
+  Ensure(ei);
+}
+
+// Constructor for an existing pointwise file and a multigroup structure.
+// This calls NWA methods to read the pointwise library and construct a
+// multigroup Compton data object, a smart pointer to which is then passed
+// to (and held by) the NWA etemp_interp class.
+Compton_NWA::Compton_NWA(const std::string &filehandle,
+                         const std::vector<double> &grp_bds, const size_t nxi) {
+
+  // make a group_data struct to pass to the lib builder:
+  multigroup::Group_data grp_data;
+  grp_data.group_bounds = grp_bds;
+  grp_data.lib_file = filehandle;
+  grp_data.n_leg = nxi;
+
+  // This is totally arbitrary, because I'm just assuming the user wants
+  // Planck-weighting...
+  grp_data.wt_func = multigroup::Weighting_function::PLANCK;
+
+  // TODO: How do we actually want to handle the weighting function?
+  // Allow the user to pass it in? Make some intelligent decision at runtime?
+
+  std::cout << "*********************************************************\n"
+            << "WARNING! Building a multigroup library from scratch might\n"
+            << " take a LOOOOOOONG time! (Don't say I didn't warn you.)  \n"
+            << "*********************************************************\n"
+            << std::endl;
+
+  multigroup_lib_builder MG_builder(grp_data);
+
+  MG_builder.build_library();
+
+  std::shared_ptr<multigroup_compton_data> Cdata = MG_builder.package_data();
+
+  Ensure(Cdata);
+
+  ei.reset(new etemp_interp(Cdata));
+
+  Ensure(ei);
+}
+
+// This method interpolates data to a given SCALED electron temperature, etemp
+// It returns the interpolated values for each g, g', and angle/Legendre moment.
+std::vector<std::vector<std::vector<double>>>
+Compton_NWA::interpolate(const double etemp) {
+
+  // Be sure the passed electron temperature is within the bounds of the lib!
+  Require(etemp >= ei->get_min_etemp());
+  Require(etemp <= ei->get_max_etemp());
+
+  std::vector<std::vector<std::vector<double>>> interped_data;
+
+  interped_data = ei->interpolate_etemp(etemp);
+
+  return interped_data;
 }
 }
--- src/compton/Compton_NWA.hh
+++ src/compton/Compton_NWA.hh
@@ -15,14 +15,44 @@
 #define __compton_Compton_NWA_hh__
 
 // C++ standard library dependencies
+#include "etemp_interp.hh"
+#include "multigroup_compton_data.hh"
 #include <iostream>
 #include <memory>
+#include <vector>
 
 namespace rtt_compton {
 
 class Compton_NWA {
+
+private:
+  std::shared_ptr<etemp_interp> ei;
+
 public:
+  //! Constructor for an existing library
   Compton_NWA(const std::string &);
+
+  Compton_NWA(const std::string &, const std::vector<double> &, const size_t);
+
+  //! Interpolation of all data to a certain electron temperature:
+  std::vector<std::vector<std::vector<double>>> interpolate(const double);
+
+  //! Retrieve group structure for the given library:
+  std::vector<double> get_group_bounds() {
+    return ei->get_Cdata()->get_group_bds();
+  }
+
+  //! Retrieve min electron temperature for the given library:
+  double get_min_etemp() { return ei->get_min_etemp(); }
+
+  //! Retrieve max electron temperature for the given library:
+  double get_max_etemp() { return ei->get_max_etemp(); }
+
+  size_t get_num_groups() { return ei->get_Cdata()->get_n_grps(); }
+  size_t get_num_xi() { return ei->get_Cdata()->get_n_xi_pts(); }
+
+  //! Retrieve electron temperature eval points (largely for diagnostic use)
+  std::vector<double> get_etemp_pts() { return ei->get_etemp_pts(); }
 };
 }
 
