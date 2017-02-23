abd2dc479142064b040e24c3bbc84278d2d1ec88
Kendra P. Keady <keadyk@lanl.gov>
2017-02-23 13:42:24 -0700

    Add a better description of the class and the NWA project, per Robs suggestion :)

--- src/compton/Compton_NWA.hh
+++ src/compton/Compton_NWA.hh
@@ -23,6 +23,41 @@
 #include "multigroup_compton_data.hh"
 
 namespace rtt_compton {
+//===========================================================================//
+/*!
+ * \class Compton_NWA
+ *
+ * \brief Provides access to relativistic Compton scattering angle and
+ * multigroup frequency distributions from the NWA project.
+ *
+ * This interface class allows the client to:
+ * 1) access (interpolate) data from existing multigroup NWA libraries
+ * 2) build new multigroup libraries from existing NWA pointwise libraries
+ * 3) obtain auxiliary information for existing multigroup libraries
+ *    (electron temperature bounds, frequency group structures, etc)
+ *
+ * This class is designed to be used with the NWA library and headers. If this
+ * are not found at the CMake configure step, the lib_compton portion of draco
+ * will not be build
+ *
+ * <b>User's environment</b>
+ *
+ * Cmake searches for the NWA library/include headers during the configuration
+ * step. The script that does this is located at:
+ *
+ * /draco/config/findNWA.cmake
+ *
+ * TODO: The NWA libs can be built with or without openMP. We need to decide
+ * which will be released (or if both will exist on a system), and ensure
+ * draco links to the correct one based on the preproc macro OPENMP_FOUND.
+ */
+
+/*!
+ * \example compton/test/tCompton_NWA.cc
+ *
+ * This unit test demonstrates the two methods for constructing a Compton_NWA
+ * object, and exercises all routines for interpolation and data access.
+*/
 
 class Compton_NWA {
 
