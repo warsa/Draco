0bc5806cfbf5835e7c3427495c8d690a0f7325ca
Kendra P. Keady <keadyk@lanl.gov>
2017-02-28 16:25:31 -0700

    Address comments by KT
    + add note to FindNWA about win logic being untested
    + remove duplicates from mark_as_advanced() list
    
    + add header blocks with date and copyright info

--- config/FindNWA.cmake
+++ config/FindNWA.cmake
@@ -1,7 +1,15 @@
+#-----------------------------*-cmake-*----------------------------------------#
+# file   config/FindNWA.cmake
+# author Kendra Keady <keadyk@lanl.gov>
+# date   2017 February 28
+# brief  Instructions for discovering the NWA vendor libraries.
+# note   Copyright (C) 2016-2017 Los Alamos National Security, LLC.
+#        All rights reserved.
+#------------------------------------------------------------------------------#
 # - Find NWA
 # Find the native NWA includes and library
 #
-#  NWA_INCLUDE_DIRS   - where to find eos_Interface.h, etc.
+#  NWA_INCLUDE_DIRS   - where to find multigroup_lib_builder.hh, etc.
 #  NWA_LIBRARIES      - List of libraries when using NWA.
 #  NWA_FOUND          - True if NWA found.
 
@@ -20,6 +28,7 @@ endif()
     NO_DEFAULT_PATH
 )
 
+# TODO: Win32 logic untested as of 2/28/17.
 if( WIN32 )
    if( NWA_STATIC )
       set( NWA_LIBRARY_NAME libcompton.lib)
@@ -45,12 +54,12 @@ if( NOT NWA_LIBRARY )
    find_path( NWA_INCLUDE_DIR    NAMES multigroup_lib_builder.hh )
    find_library(NWA_LIBRARY      NAMES ${NWA_LIBRARY_NAME} )
 endif()
-mark_as_advanced( NWA_LIBRARY NWA_INCLUDE_DIR NWA_LIBRARY )
+mark_as_advanced( NWA_LIBRARY NWA_INCLUDE_DIR )
 
 # handle the QUIETLY and REQUIRED arguments and set NWA_FOUND to TRUE if 
-# all listed variables are TRUE (removed NWA_INCLUDE_DIR)
+# all listed variables are TRUE
 include(FindPackageHandleStandardArgs)
-find_package_handle_standard_args(NWA DEFAULT_MSG NWA_LIBRARY)
+find_package_handle_standard_args(NWA DEFAULT_MSG NWA_INCLUDE_DIR NWA_LIBRARY)
 
 if (NWA_FOUND)
    set(NWA_FOUND ${NWA_FOUND} CACHE BOOL "Did we find the NWA libraries?")
