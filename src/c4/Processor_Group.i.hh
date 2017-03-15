c5cef61c1edb48232fe3b304d93ff9ded648cca8
Kent G. Budge <kgbudge@lanl.gov>
2017-03-15 13:29:20 -0600

    Extend c4::Processor_Group::sum to operate on any random access container.
    The random access container must conform to the C++11 conventions for such a containter. In particular, the value_type must be defined and &x[0] must return a pointer to T pointing to the beginning of the raw storage underlying the container.
    
    This is needed to support use of Teuchos::ArrayView and Teuchos::ArrayRCP in the Capsaicin project.

--- src/c4/Processor_Group.i.hh
+++ src/c4/Processor_Group.i.hh
@@ -22,9 +22,12 @@
 
 namespace rtt_c4 {
 //---------------------------------------------------------------------------//
-template <typename T> void Processor_Group::sum(std::vector<T> &x) {
+template <typename RandomAccessContainer>
+void Processor_Group::sum(RandomAccessContainer &x) {
+  typedef typename RandomAccessContainer::value_type T;
+
   // copy data into send buffer
-  std::vector<T> y = x;
+  std::vector<T> y(x.begin(), x.end());
 
   // do global MPI reduction (result is on all processors) into x
   int status =
