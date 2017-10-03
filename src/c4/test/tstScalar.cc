2a633c3a936fc130b7b4c407dc4327193aefa04b
Kelly (KT) Thompson <kgt@lanl.gov>
2017-10-02 18:02:43 -0600

    Allow c4 to build when DRACO_C4=SCALAR.
    + While reviewing #280, I discovered that Draco no-MPI builds were broken.  This
      PR fixes `develop` for no-MPI builds and should be merged before #280.
    + Update function signature for `T prefix_sum` found in `C4_Serial.hh` to use
      the same argument types as the function of the same name found in
      `C4_MPI.hh`. This fixes the build error `call of overloaded function is
      ambiguous`.
    + Update `tstScalar.cc` to check the value of `request.comlete()` instead of
      `request.count()` since the later member function no longer exists.
    + Clean up comments.

--- src/c4/test/tstScalar.cc
+++ src/c4/test/tstScalar.cc
@@ -5,10 +5,7 @@
  * \date   Tue Nov  1 13:24:19 2005
  * \brief  Test functions provided by C4_Serial.cc/.hh
  * \note   Copyright (C) 2016-2017 Los Alamos National Security, LLC.
- *         All rights reserved.
- */
-//---------------------------------------------------------------------------//
-// $Id$
+ *         All rights reserved. */
 //---------------------------------------------------------------------------//
 
 #include "c4/ParallelUnitTest.hh"
@@ -102,7 +99,7 @@ void tstScalar(rtt_dsxx::UnitTest &ut) {
   request = rtt_c4::receive_async(&int3, 1, 0, 0);
   rtt_c4::send_async(request, &int3, 1, 0, 0);
 
-  if (request.inuse() == 0 && request.count() == 0) {
+  if (request.inuse() == 0 && request.complete()) {
     PASSMSG("For --with-c4=scalar, successful test of send_async(const "
             "T*,int,int,int) and receive_async( C4_Req&,T*,int,int,int).");
   } else {
@@ -116,7 +113,6 @@ void tstScalar(rtt_dsxx::UnitTest &ut) {
 }
 
 //---------------------------------------------------------------------------//
-
 int main(int argc, char *argv[]) {
   rtt_c4::ParallelUnitTest ut(argc, argv, rtt_dsxx::release);
   try {
