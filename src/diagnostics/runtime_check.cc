5e45711a1c3df323c0feedf0b36b9677db046e43
Kelly (KT) Thompson <kgt@lanl.gov>
2018-04-02 16:23:05 -0600

    Fix formatting issues.

--- src/diagnostics/runtime_check.cc
+++ src/diagnostics/runtime_check.cc
@@ -8,8 +8,8 @@
  *         All rights reserved. */
 //---------------------------------------------------------------------------//
 
-#include "c4/C4_Functions.hh"
 #include "diagnostics/runtime_check.hh"
+#include "c4/C4_Functions.hh"
 #include "ds++/DracoStrings.hh"
 #include <exception>
 
@@ -24,8 +24,8 @@ void runtime_check(bool condition, char const *message) noexcept(false) {
   if (sum != 0) // some processors failed the condition
   {
     throw std::runtime_error("runtime error detected on " +
-                             rtt_dsxx::to_string(sum) +
-                             " processor(s): " + message);
+                             rtt_dsxx::to_string(sum) + " processor(s): " +
+                             message);
   }
 }
 
