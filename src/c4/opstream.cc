d868ed1bd9a62fd5e916322da4d342791ef0d7e7
Kent G. Budge <kgbudge@lanl.gov>
2018-05-31 15:13:36 -0600

    Add a parallel output stream.
    Define a class opstream and a global object of this class, pout, corresopnding to cout. This class behaves like
    any other output stream in the C++ stream library, with full support for formatting of output, manipulators,
    unformatted output, &c. However, output inserted by any MPI rank into the stream is buffered and does not
    appear on the console (cout) until the send() function is called on pout. At this point, all the buffered output
    for rank 0 appears at the console, followed by all the output for rank 1, and so on. There is absolutely no
    requirement that the output from any rank be similar to that from any other rank, or that a rank produce any
    output at all. However, the send() function *must* be called synchronously.
    
    This class is meant to have some of the functionality of the old Spin_Lock class, which was designed to serialize
    sections of code. The most common use case turned out to be serializing output, which Spin_Lock *did not actually
    achieve*. For this reason, Spin_Lock was abandoned.
    
    The new opstream class is not useful except for serializing output, but it actually succeeds in this, and this
    is a very important use case for code serialization.

---
+++ src/c4/opstream.cc
@@ -0,0 +1,63 @@
+//----------------------------------*-C++-*----------------------------------//
+/*!
+ * \file   c4/opstream.cc
+ * \author Mike Buksas
+ * \date   Thu May  1 14:42:10 2008
+ * \brief
+ * \note   Copyright (C) 2016-2018 Los Alamos National Security, LLC.
+ *         All rights reserved.
+ */
+//---------------------------------------------------------------------------//
+
+//---------------------------------------------------------------------------//
+
+#include <iostream>
+
+#include "C4_Functions.hh"
+#include "opstream.hh"
+
+namespace rtt_c4 {
+using namespace std;
+
+opstream pout;
+
+//---------------------------------------------------------------------------//
+void opstream::mpibuf::send() {
+  unsigned const pid = rtt_c4::node();
+  if (pid == 0) {
+    buffer_.push_back('\0');
+    cout << &buffer_[0];
+    buffer_.clear();
+
+    unsigned const pids = rtt_c4::nodes();
+    for (unsigned i = 1; i < pids; ++i) {
+      unsigned N;
+      receive(&N, 1, i);
+      buffer_.resize(N);
+      rtt_c4::receive(&buffer_[0], N, i);
+      buffer_.push_back('\0');
+      cout << &buffer_[0];
+    }
+  } else {
+    unsigned N = buffer_.size();
+    rtt_c4::send(&N, 1, 0);
+    rtt_c4::send(&buffer_[0], N, 0);
+  }
+  buffer_.clear();
+  rtt_c4::global_barrier();
+}
+
+//---------------------------------------------------------------------------//
+/*virtual*/ opstream::mpibuf::int_type opstream::mpibuf::overflow(int_type c) {
+  buffer_.push_back(c);
+  return c;
+}
+
+//---------------------------------------------------------------------------//
+void opstream::mpibuf::shrink_to_fit() { buffer_.shrink_to_fit(); }
+
+} // end namespace rtt_c4
+
+//---------------------------------------------------------------------------//
+// end of opstream.cc
+//---------------------------------------------------------------------------//
