dc3d4afb6a69b19697ca6af1ae62ab579ad6dd5d
Kelly (KT) Thompson <kgt@lanl.gov>
2018-06-26 21:43:12 -0600

    Re-enable a baseline (no MPI) Travis build.
    + Move definition of TPL versions to `.travis.yml`.
    + Do not try to build openmpi (for now) and configure using
      `-DDRACO_C4=SCALAR`.
    + Switch the Travis build from gcc-6 to gcc-7.
    + No longer install gsl, lapack or blas via apt-get.
    + Begin installing modules, doxygen and graphviz via apt-get.
    + Re-enable the `WERROR=ON` matrix option and code coverage tracking.
    + Disable some gcc `-fsanitize` options that seem to confuse Travis. Also, do
      not pass the `-fopenmp` to the linker.

--- .travis-install-dependencies.sh
+++ .travis-install-dependencies.sh
@@ -18,12 +18,12 @@ topdir=`pwd` # /home/travis/build/lanl/Draco
 # HOME = /home/travis
 # USER = travis
 # GROUP = travis
-RANDOM123_VER=1.09
-CMAKE_VERSION=3.9.0-Linux-x86_64
-NUMDIFF_VER=5.8.1
-CLANG_FORMAT_VER=6.0
-DISTRO=trusty
-OPENMPI_VER=1.10.5
+# RANDOM123_VER=1.09
+# CMAKE_VERSION=3.9.0-Linux-x86_64
+# NUMDIFF_VER=5.8.1
+# CLANG_FORMAT_VER=6.0
+# DISTRO=trusty
+# OPENMPI_VER=1.10.5
 
 # Return integer > 0 if 'develop' branch is found.
 function find_dev_branch
@@ -69,39 +69,49 @@ if [[ ${STYLE} ]]; then
   fi
   run "cd $topdir"
 
-# else
+else
 
-  # # Random123
-  # echo " "
-  # echo "Random123"
-  # cd $HOME
-  # run "wget http://www.deshawresearch.com/downloads/download_random123.cgi/Random123-${RANDOM123_VER}.tar.gz"
-  # run "tar -xvf Random123-${RANDOM123_VER}.tar.gz &> build-r123.log"
-  # echo "Please set RANDOM123_INC_DIR=$HOME/Random123-${RANDOM123_VER}/include"
-  # run "ls $HOME/Random123-${RANDOM123_VER}/include"
+  # Random123
+  echo " "
+  echo "Random123"
+  cd $HOME
+  run "wget -q http://www.deshawresearch.com/downloads/download_random123.cgi/Random123-${RANDOM123_VER}.tar.gz"
+  run "tar -xvf Random123-${RANDOM123_VER}.tar.gz &> build-r123.log"
+  echo "Please set RANDOM123_INC_DIR=$HOME/Random123-${RANDOM123_VER}/include"
+  run "ls $HOME/Random123-${RANDOM123_VER}/include"
 
-  # # CMake
-  # echo " "
-  # echo "CMake"
-  # run "cd $HOME"
-  # run "wget --no-check-certificate http://www.cmake.org/files/v${CMAKE_VERSION:0:3}/cmake-${CMAKE_VERSION}.tar.gz"
-  # run "tar -xzf cmake-${CMAKE_VERSION}.tar.gz &> build-cmake.log"
-  # run "cd $topdir"
+  # CMake
+  echo " "
+  echo "CMake"
+  run "cd $HOME"
+  run "wget -q --no-check-certificate http://www.cmake.org/files/v${CMAKE_VERSION:0:3}/cmake-${CMAKE_VERSION}.tar.gz"
+  run "tar -xzf cmake-${CMAKE_VERSION}.tar.gz &> build-cmake.log"
+  run "cd $topdir"
 
-  # # Numdiff
-  # echo " "
-  # echo "Numdiff"
-  # run "wget http://mirror.lihnidos.org/GNU/savannah/numdiff/numdiff-${NUMDIFF_VER}.tar.gz"
-  # run "tar -xvf numdiff-${NUMDIFF_VER}.tar.gz >& build-numdiff.log"
-  # run "cd numdiff-${NUMDIFF_VER}"
-  # run "./configure --prefix=/usr && make >> build-numdiff.log 2>&1"
-  # run "sudo make install"
-  # run "cd $topdir"
+  # Numdiff
+  echo " "
+  echo "Numdiff"
+  run "wget -q http://mirror.lihnidos.org/GNU/savannah/numdiff/numdiff-${NUMDIFF_VER}.tar.gz"
+  run "tar -xvf numdiff-${NUMDIFF_VER}.tar.gz >& build-numdiff.log"
+  run "cd numdiff-${NUMDIFF_VER}"
+  run "./configure --prefix=/usr && make >> build-numdiff.log 2>&1"
+  run "sudo make install"
+  run "cd $topdir"
+
+  # GSL
+  echo " "
+  echo "GSL"
+  run "wget -q http://mirror.switch.ch/ftp/mirror/gnu/gsl/gsl-2.4.tar.gz"
+  run "tar -xvf gsl-2.4.tar.gz &> build-gsl.log"
+  run "cd gsl-2.4"
+  run "./configure --prefix=/usr && make >> build-gsl.log 2>&1"
+  run "sudo make install"
+  run "cd $topdir"
 
   # # OpenMPI
   # echo " "
   # echo "OpenMPI"
-  # run "wget --no-check-certificate https://www.open-mpi.org/software/ompi/v1.10/downloads/openmpi-${OPENMPI_VER}.tar.gz"
+  # run "wget -q --no-check-certificate https://www.open-mpi.org/software/ompi/v1.10/downloads/openmpi-${OPENMPI_VER}.tar.gz"
   # run "tar -zxf openmpi-${OPENMPI_VER}.tar.gz > build-openmpi.log"
   # run "cd openmpi-${OPENMPI_VER}"
   # run "./configure --enable-mpi-thread-multiple --quiet >> build-openmpi.log 2>&1"
--- .travis-run-tests.sh
+++ .travis-run-tests.sh
@@ -17,61 +17,68 @@ topdir=`pwd` # /home/travis/build/lanl/Draco
 # HOME = /home/travis
 # USER = travis
 # GROUP = travis
-RANDOM123_VER=1.09
-CMAKE_VERSION=3.9.0-Linux-x86_64
-NUMDIFF_VER=5.8.1
-CLANG_FORMAT_VER=3.9
-OPENMPI_VER=1.10.5
-GCCVER=6
+# RANDOM123_VER=1.09
+# CMAKE_VERSION=3.9.0-Linux-x86_64
+# NUMDIFF_VER=5.8.1
+# CLANG_FORMAT_VER=3.9
+# OPENMPI_VER=1.10.5
+# GCCVER=6
 export CXX=`which g++-${GCCVER}`
 export CC=`which gcc-${GCCVER}`
 export FC=`which gfortran-${GCCVER}`
 
 if [[ ${STYLE} ]]; then
   regression/check_style.sh -t
-# else
-#   mkdir -p build
-#   cd build
-#   # configure
-#   # -Wl,--no-as-needed is a workaround for bugs.debian.org/457284 .
-#   echo " "
-#   echo "${CMAKE} -DCMAKE_EXE_LINKER_FLAGS=\"-Wl,--no-as-needed\" .."
-#   ${CMAKE} -DCMAKE_EXE_LINKER_FLAGS="-Wl,--no-as-needed" ..
-#   error_code=$?
-#   # if configure was successful, the start the build, otherwise abort.
-#   if [[ $error_code -eq 0 ]]; then
-#     echo " "
-#     echo "make -j 2 VERBOSE=1"
-#     make -j 2 VERBOSE=1
-#     error_code=$?
-#   else
-#     echo "configure failed, errorcode=$error_code"
-#     exit $error_code
-#   fi
-#   # if the build was successful, then run the tests, otherwise abort.
-#   if [[ $error_code -eq 0 ]]; then
-#     echo " "
-#     echo "${CTEST} -VV -E \(c4_tstOMP_2\)"
-#     ${CTEST} -VV -E \(c4_tstOMP_2\)
-#     error_code=$?
-#   else
-#     echo "build failed, errorcode=$error_code"
-#     exit $error_code
-#   fi
-#   # if some of the tests failed, rerun them with full verbosity to provide more
-#   # information in the output log.
-#   # if ! [[ $error_code -eq 0 ]]; then
-#   #   echo "some tests failed, errorcode=$error_code, trying again with more verbosity."
-#   #   echo " "
-#   #   echo "${CTEST} --rerun-failed -VV -E \(c4_tstOMP_2\)"
-#   #   ${CTEST} --rerun-failed -VV -E \(c4_tstOMP_2\)
-#   #   exit $error_code
-#   #   echo "error_code=$error_code"
-#   # fi
-#   echo " "
-#   echo "make install DESTDIR=${HOME}"
-#   make install DESTDIR="${HOME}"
-#   cd -
+else
+  mkdir -p build
+  cd build
+  # configure
+  # -Wl,--no-as-needed is a workaround for bugs.debian.org/457284 .
+  echo " "
+  if [[ -f CMakeCache.txt ]]; then
+    echo "===== CMakeCache.txt ====="
+    cat CMakeCache.txt
+  fi
+  echo "========"
+  echo "${CMAKE} -DDRACO_C4=SCALAR .."
+  ${CMAKE} -DDRACO_C4=SCALAR ..
+#  echo "${CMAKE} -DCMAKE_EXE_LINKER_FLAGS=\"-Wl,--no-as-needed\" -DDRACO_C4=SCALAR .."
+#  ${CMAKE} -DCMAKE_EXE_LINKER_FLAGS="-Wl,--no-as-needed" -DDRACO_C4=SCALAR ..
+  error_code=$?
+  # if configure was successful, the start the build, otherwise abort.
+  if [[ $error_code -eq 0 ]]; then
+    echo " "
+    echo "make -j 2 VERBOSE=1"
+    make -j 2 VERBOSE=1
+    error_code=$?
+  else
+    echo "configure failed, errorcode=$error_code"
+    exit $error_code
+  fi
+  # if the build was successful, then run the tests, otherwise abort.
+  if [[ $error_code -eq 0 ]]; then
+    echo " "
+    echo "${CTEST} -VV -E \(c4_tstOMP_2\)"
+    ${CTEST} -VV -E \(c4_tstOMP_2\)
+    error_code=$?
+  else
+    echo "build failed, errorcode=$error_code"
+    exit $error_code
+  fi
+  # if some of the tests failed, rerun them with full verbosity to provide more
+  # information in the output log.
+  # if ! [[ $error_code -eq 0 ]]; then
+  #   echo "some tests failed, errorcode=$error_code, trying again with more verbosity."
+  #   echo " "
+  #   echo "${CTEST} --rerun-failed -VV -E \(c4_tstOMP_2\)"
+  #   ${CTEST} --rerun-failed -VV -E \(c4_tstOMP_2\)
+  #   exit $error_code
+  #   echo "error_code=$error_code"
+  # fi
+  echo " "
+  echo "make install DESTDIR=${HOME}"
+  make install DESTDIR="${HOME}"
+  cd -
 fi
 
 # Finish up and report
