59164d51a2a2414eaebd602ed48fde253c8db0da
Kelly (KT) Thompson <kgt@lanl.gov>
2018-09-13 14:04:45 -0600

    Improve mesh_element and norms code coverage
    + by adding tests or removing features.
    + In `Element_Definition.cc`, comment out `construct_pentagon()` as it is not
      tested and apparently not used.
    + In `Norms_Index`, comment out `reset()` as it is not tested and apparently not
      used.

--- src/norms/Norms_Index.t.cc
+++ src/norms/Norms_Index.t.cc
@@ -1,14 +1,11 @@
 //----------------------------------*-C++-*----------------------------------//
 /*!
-  \file   Norms_Index.t.cc
-  \author Rob Lowrie
-  \date   Fri Jan 14 13:00:47 2005
-  \brief  Instantiates Norms_Index for some types.
+ * \file   Norms_Index.t.cc
+ * \author Rob Lowrie
+ * \date   Fri Jan 14 13:00:47 2005
+ * \brief  Instantiates Norms_Index for some types.
  * \note   Copyright (C) 2016-2018 Los Alamos National Security, LLC.
- *         All rights reserved.
-*/
-//---------------------------------------------------------------------------//
-
+ *         All rights reserved. */
 //---------------------------------------------------------------------------//
 
 #include "Norms_Index.t.hh"
@@ -20,6 +17,7 @@ namespace rtt_norms {
 template class Norms_Index<size_t>;
 template class Norms_Index<Index_Labeled>;
 template class Norms_Index<Index_Proc>;
+
 } // namespace rtt_norms
 
 //---------------------------------------------------------------------------//
