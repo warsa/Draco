51b6afe3721dfa827c0851090cf3af5ef7dcc00c
Kelly (KT) Thompson <kgt@lanl.gov>
2018-09-24 20:46:36 -0600

    Avoid compiling for AVX2 instructions on older hardware.
    + The compile option `/arch:AVX2` was always enabled for Visual Studio, but
      pre-Haswell hardware does not support AVX2.  While the compiler could still
      create code, it would fail to run properly. To avoid this, we add a
      platform check for AVX2 capabilities -- actually the check already existed
      in `config/query_fma.cc` but it needed a small change to narrow the check
      to AVX2 -nly.
    + Modify `query_fma.cc` to take command line arguments `-f` and `-a`, which
      limit the check to fma-availability or avx2-availablity only. This code
      will return `1` if those features are found.
    + Update `platform_checks.cmake` to compile and run `query_fma.cc` and then
      set the build variables `HAVE_HARDWARE_AVX2` and `HAVE_HARDWARE_FMA`.
      These values are saved to `ds++/config.h` and are also available when
      the build system parses `windows-cl.cmake` so that appropriate code
      paths can be taken.
    + Also:
      + Fix a comment block in `drel.f90`.
      + Fix a typo in `appveyor.yml` that prevented output from failing tests
        from being printed.

--- config/windows-cl.cmake
+++ config/windows-cl.cmake
@@ -7,6 +7,9 @@
 #        All rights reserved.
 #------------------------------------------------------------------------------#
 
+# Useful reference information:
+# https://docs.microsoft.com/en-us/cpp/intrinsics/cpuid-cpuidex?view=vs-2017
+
 #
 # Sanity Checks
 #
@@ -62,7 +65,10 @@ if( NOT CXX_FLAGS_INITIALIZED )
   # - /std:c++14 (should be added by cmake in compilerEnv.cmake)
   # - /showIncludes
   # - /FC
-  set( CMAKE_C_FLAGS "/W2 /Gy /fp:precise /arch:AVX2 /DWIN32 /D_WINDOWS /MP /wd4251" )
+  set( CMAKE_C_FLAGS "/W2 /Gy /fp:precise /DWIN32 /D_WINDOWS /MP /wd4251" )
+  if(HAVE_HARDWARE_AVX2)
+    string(APPEND CMAKE_C_FLAGS " /arch:AVX2")
+  endif()
   set( CMAKE_C_FLAGS_DEBUG "/${MD_or_MT_debug} /Od /Zi /DDEBUG /D_DEBUG" )
   set( CMAKE_C_FLAGS_RELEASE "/${MD_or_MT} /O2 /DNDEBUG" )
   set( CMAKE_C_FLAGS_MINSIZEREL "/${MD_or_MT} /O1 /DNDEBUG" )
