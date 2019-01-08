786d4da920618d6e4738f654eca1aeb3a505856c
Kelly (KT) Thompson <kgt@lanl.gov>
2019-01-08 16:18:50 -0700

    Fix a module load error
    On machines where group 'ccsrad' doesn't exist, modules ndi and csk were not
    being loaded.  Update logic to do the right thing.

--- src/ds++/FMA.hh
+++ src/ds++/FMA.hh
@@ -35,6 +35,9 @@
  *   return value;
  * }
  * \endcode
+ *
+ * \sa [What Every Computer Scientist Should Know About Floating-Point
+ * Arithmetic](https://docs.oracle.com/cd/E19957-01/806-3568/ncg_goldberg.html)
  */
 //---------------------------------------------------------------------------//
 
