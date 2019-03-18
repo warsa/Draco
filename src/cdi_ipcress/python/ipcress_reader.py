019288bf3e66281a7f5cd97e4c4af3482d0d9cd4
Alex Long <along@lanl.gov>
2019-03-18 13:59:39 -0600

    Tweak Ipcress reader for python 3.0
    + Encode the string values read from the binary file with UTF 8 to reproduce old results
    + Add the ability to write data to an ipcress file for a material and property field
    + Add a file that demos the new write feature
    + Change names of demo files to start with "demo"
    + Print the number of groups and group structure by default

--- src/cdi_ipcress/python/ipcress_reader.py
+++ src/cdi_ipcress/python/ipcress_reader.py
@@ -16,6 +16,7 @@
 import re
 from numpy import arange, sin, pi, min, max
 import sys
+import struct
 import numpy as np
 from struct import *
 from math import *
@@ -38,6 +39,16 @@ def get_data_for_id(filename, data_start_index, num_entries):
   return temp_grid
 ################################################################################
 
+
+################################################################################
+def write_data_for_id(filename, data_start_index, num_entries, new_values):
+  # "wb" is write binary mode
+  with open(filename, "r+b") as f:
+    f.seek(data_start_index*8)
+    for i in range(num_entries):
+      s = struct.pack('>d', new_values[i])
+      f.write(s)
+################################################################################
 
 ################################################################################
 def interpolate_mg_opacity_data(T_grid, rho_grid, hnu_grid, op_data, \
@@ -170,7 +181,7 @@ def read_information_from_file(ipcress_file):
   #print("Beginnging of data: {0}".format(toc_int[0]))
   #print("Max records: {0} , max search keys: {1}".format(mxrec, mxkey))
 
-  property = []
+  mat_property = []
 
   ds = []
   dfo = []
@@ -211,13 +222,13 @@ def read_information_from_file(ipcress_file):
       temp_property = []
       for j in range(mxkey):
         three_string = []
-        three_string.append( f.read(8))
-        three_string.append( f.read(8))
-        three_string.append( f.read(8))
+        three_string.append( f.read(8).decode("utf-8"))
+        three_string.append( f.read(8).decode("utf-8"))
+        three_string.append( f.read(8).decode("utf-8"))
         if (j==0): temp_property.append(three_string[2].strip() )
         elif (j==1): temp_property.append(three_string[0].strip())
         else: temp_property.append(i) #index of data table containing values
-      property.append(temp_property)
+      mat_property.append(temp_property)
 
   materials = []
   for m in range(num_mats):
@@ -232,8 +243,46 @@ def read_information_from_file(ipcress_file):
   #  print(i)
 
   #return the list of available properties, data file offsets and data sizes
-  return materials, property, dfo, ds
+  return materials, mat_property, dfo, ds
 ################################################################################
+
+
+###############################################################################
+def write_information_to_file(ipcress_file, material_ID, mat_property, new_values):
+  materials, property_list, dfo, ds = read_information_from_file(ipcress_file)
+  # check to make sure material is in file
+  material_IDs = []
+  for imat in materials:
+    material_IDs.append(str(imat[1]))
+  if (not (material_ID in material_IDs)):
+    print("ERROR: Material ID not found in file, not changing anything!")
+    return
+
+  # try to find property in file
+  property_found = False
+  propery_index = 0
+  for prop_i, prop in enumerate(property_list):
+    if (material_ID == prop[0] and mat_property == prop[1]):
+      property_found = True
+      property_index = prop_i
+      break
+
+  # make sure sizes match of property you're about to write
+  if (property_found and  ds[property_index+1] != len(new_values)):
+    print("ERROR: Number of new values does not match size of old values, not changing anything!")
+    return
+
+  # if the combination of property and material was found, write the new data to
+  # the ipcress file
+  if property_found:
+    write_data_for_id( ipcress_file, dfo[property_index+1], \
+      ds[property_index+1], new_values)
+  else:
+    print("ERROR: Combination of material ID and property not found, not changing anything!")
+  return
+################################################################################
+
+
 
 ################################################################################
 # Checks to see if there are any zeros in the opcaity data--zero data is
