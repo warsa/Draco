7430637fa491ff1aaf750f2bb42f6226bd3ec07b
Mathew Cleveland <cleveland@lanl.gov>
2019-03-19 16:21:36 -0600

    + Add analytic ei_coupling to CDI

--- src/cdi/CDI.hh
+++ src/cdi/CDI.hh
@@ -11,6 +11,7 @@
 #ifndef rtt_cdi_CDI_hh
 #define rtt_cdi_CDI_hh
 
+#include "EICoupling.hh"
 #include "EoS.hh"
 #include "GrayOpacity.hh"
 #include "MultigroupOpacity.hh"
@@ -464,6 +465,7 @@ class DLL_PUBLIC_cdi CDI {
   typedef std::shared_ptr<const MultigroupOpacity> SP_MultigroupOpacity;
   typedef std::shared_ptr<const OdfmgOpacity> SP_OdfmgOpacity;
   typedef std::shared_ptr<const EoS> SP_EoS;
+  typedef std::shared_ptr<const EICoupling> SP_EICoupling;
   typedef std::vector<SP_GrayOpacity> SF_GrayOpacity;
   typedef std::vector<SF_GrayOpacity> VF_GrayOpacity;
   typedef std::vector<SP_MultigroupOpacity> SF_MultigroupOpacity;
@@ -530,6 +532,15 @@ class DLL_PUBLIC_cdi CDI {
    */
   SP_EoS spEoS;
 
+  /*!
+   * \brief Smart pointer to the electron-ion coupling object.
+   *
+   * spEICoupling is a smart pointer that links a CDI object to an electron-ion
+   * coupling object (any type of Analytic, etc.).  The pointer is
+   * established in the CDI constructor.
+   */
+  SP_EICoupling spEICoupling;
+
   //! Material ID.
   std_string matID;
 
@@ -571,6 +582,9 @@ public:
   //! Register an EOS (rtt_cdi::Eos) with CDI.
   void setEoS(const SP_EoS &in_spEoS);
 
+  //! Register an EICoupling (rtt_cdi::EICoupling) with CDI.
+  void setEICoupling(const SP_EICoupling &in_spEICoupling);
+
   //! Clear all data objects
   void reset();
 
@@ -581,6 +595,7 @@ public:
   SP_MultigroupOpacity mg(rtt_cdi::Model m, rtt_cdi::Reaction r) const;
   SP_OdfmgOpacity odfmg(rtt_cdi::Model m, rtt_cdi::Reaction r) const;
   SP_EoS eos(void) const;
+  SP_EICoupling ei_coupling(void) const;
 
   //! Collapse Multigroup data to single-interval data with Planck weighting.
   static double
@@ -643,6 +658,7 @@ public:
   bool isMultigroupOpacitySet(rtt_cdi::Model, rtt_cdi::Reaction) const;
   bool isOdfmgOpacitySet(rtt_cdi::Model, rtt_cdi::Reaction) const;
   bool isEoSSet() const;
+  bool isEICouplingSet() const;
 
   //! Copies the vector of the stored frequency group boundary vector
   static std::vector<double> getFrequencyGroupBoundaries();
---
+++ src/cdi_analytic/Analytic_EICoupling.hh
@@ -0,0 +1,93 @@
+//----------------------------------*-C++-*----------------------------------//
+/*!
+ * \file   cdi_analytic/Analytic_EICoupling.hh
+ * \author Thomas M. Evans
+ * \date   Tue Oct  2 16:22:32 2001
+ * \brief  Analytic_EICoupling class definition.
+ * \note   Copyright (C) 2016-2019 Triad National Security, LLC.
+ *         All rights reserved. */
+//---------------------------------------------------------------------------//
+
+#ifndef __cdi_analytic_Analytic_EICoupling_hh__
+#define __cdi_analytic_Analytic_EICoupling_hh__
+
+#include "Analytic_Models.hh"
+#include "cdi/EICoupling.hh"
+#include <memory>
+
+namespace rtt_cdi_analytic {
+
+//===========================================================================//
+/*!
+ * \class Analytic_EICoupling
+ *
+ * \brief Derived rtt_cdi::EICoupling class for analytic electron-ion 
+ * coupling data.
+ *
+ * The Analytic_EICoupling class is a derived rtt_cdi::EICoupling class. It provides
+ * analytic electron-ion coupling data.  The specific analytic EICoupling model is
+ * derived from the rtt_cdi_analytic::Analytic_EICoupling_Model base class.  Several
+ * pre-built derived classes are provided in Analytic_Models.hh.
+ *
+ * Clients of this class can provide any analytic model class as long as it
+ * conforms to the rtt_cdi_analytic::Analytic_EICoupling_Model interface.
+ *
+ * See the member functions for details about the data types and units.
+ *
+ * \example cdi_analytic/test/tstAnalytic_EICoupling.cc
+ *
+ * Example usage of Analytic_EICoupling, Analytic_EICoupling_Model, and their incorporation
+ * into rtt_cdi::CDI.
+ */
+// revision history:
+// -----------------
+// 0) original
+//
+//===========================================================================//
+
+class DLL_PUBLIC_cdi_analytic Analytic_EICoupling : public rtt_cdi::EICoupling {
+public:
+  // Useful typedefs.
+  typedef std::shared_ptr<Analytic_EICoupling_Model> SP_Analytic_Model;
+  typedef std::shared_ptr<const Analytic_EICoupling_Model> const_SP_Model;
+  typedef std::vector<double> sf_double;
+  typedef std::vector<char> sf_char;
+
+private:
+  // Analytic EICoupling model.
+  SP_Analytic_Model analytic_model;
+
+public:
+  // Constructor.
+  explicit Analytic_EICoupling(SP_Analytic_Model model_in);
+
+  // Unpacking constructor.
+  explicit Analytic_EICoupling(const sf_char &);
+
+  // >>> ACCESSORS
+  const_SP_Model get_Analytic_Model() const { return analytic_model; }
+
+  // >>> INTERFACE SPECIFIED BY rtt_cdi::EoS
+
+  // Get electron ion coupling.
+  double getElectronIonCoupling(const double eTemperature,
+                                const double iTemperature, const double density,
+                                const double w_e, const double w_i) const;
+
+  sf_double getElectronIonCoupling(const sf_double &eTemperature,
+                                   const sf_double &iTemperature,
+                                   const sf_double &density,
+                                   const sf_double &w_e,
+                                   const sf_double &w_i) const;
+
+  // Pack the Analytic_EICoupling into a character string.
+  sf_char pack() const;
+};
+
+} // end namespace rtt_cdi_analytic
+
+#endif // __cdi_analytic_Analytic_EICoupling_hh__
+
+//---------------------------------------------------------------------------//
+// end of cdi_analytic/Analytic_EICoupling.hh
+//---------------------------------------------------------------------------//
