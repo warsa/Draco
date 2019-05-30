e4704f1874ef9f56f0f96799d7227273a061fbb9
Kelly (KT) Thompson <kgt@lanl.gov>
2019-05-30 10:36:10 -0600

    Update dev and regress environment for Cray machines.
    Background:
    
    + In recent commits we have moved to python3, but on cray we need a little bit
      of extra setup to get a working python3.
    + Recent updates to the Cray Programming Environment also requires additional
      srun options to allow tests to run simultaneously in a single allocation.
    
    Changes:
    
    + Load the python/3.6-anaconda-5.0.1 module by default so that the 'xml' python
      module can be found.  This is required by the MCGRID parser.  This is required
      for both developer and regression environments.
    + Add the `--gres=craynetwork:0` srun option to support running multiple tests
      simultaneously in a single allocation (shared node mode).

--- config/setupMPI.cmake
+++ config/setupMPI.cmake
@@ -337,7 +337,7 @@ macro( setupCrayMPI )
   #           stomp on the same cores.
 
   set(postflags "-N 1 --cpu_bind=verbose,cores ")
-  string(APPEND postflags " --exclusive")
+  string(APPEND postflags " --exclusive --gres=craynetwork:0")
   set( MPIEXEC_POSTFLAGS ${postflags} CACHE STRING
     "extra mpirun flags (list)." FORCE)
 
